51157cceeb24f70b759e7ad7526372ab2a5b24cf
Matias Manavella <matiasm@nimble.la>
2020-08-05 11:21:17 -0300

    Copy collector enhanced

--- pkg/collect/copy.go
+++ pkg/collect/copy.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	troubleshootv1beta1 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta1"
 	corev1 "k8s.io/api/core/v1"
@@ -56,12 +57,13 @@ func Copy(c *Collector, copyCollector *troubleshootv1beta1.Copy) (map[string][]b
 }
 
 func copyFiles(c *Collector, client *kubernetes.Clientset, pod corev1.Pod, copyCollector *troubleshootv1beta1.Copy) (map[string][]byte, map[string]string) {
+	files := make(map[string][]byte)
 	container := pod.Spec.Containers[0].Name
 	if copyCollector.ContainerName != "" {
 		container = copyCollector.ContainerName
 	}
 
-	command := []string{"cat", copyCollector.ContainerPath}
+	command := []string{"find", copyCollector.ContainerPath, "-type", "f"}
 
 	req := client.CoreV1().RESTClient().Post().Resource("pods").Name(pod.Name).Namespace(pod.Namespace).SubResource("exec")
 	scheme := runtime.NewScheme()
@@ -96,6 +98,9 @@ func copyFiles(c *Collector, client *kubernetes.Clientset, pod corev1.Pod, copyC
 		Stderr: &stderr,
 		Tty:    false,
 	})
+
+	filepaths := strings.Split(output.String(), "\n")
+
 	if err != nil {
 		errors := map[string]string{
 			filepath.Join(copyCollector.ContainerPath, "error"): err.Error(),
@@ -109,9 +114,59 @@ func copyFiles(c *Collector, client *kubernetes.Clientset, pod corev1.Pod, copyC
 		return nil, errors
 	}
 
-	return map[string][]byte{
-		copyCollector.ContainerPath: output.Bytes(),
-	}, nil
+	for _, file := range filepaths {
+		if file != "" {
+			command := []string{"cat", file}
+
+			req := client.CoreV1().RESTClient().Post().Resource("pods").Name(pod.Name).Namespace(pod.Namespace).SubResource("exec")
+			scheme := runtime.NewScheme()
+			if err := corev1.AddToScheme(scheme); err != nil {
+				return nil, map[string]string{
+					filepath.Join(copyCollector.ContainerPath, "error"): err.Error(),
+				}
+			}
+			parameterCodec := runtime.NewParameterCodec(scheme)
+			req.VersionedParams(&corev1.PodExecOptions{
+				Command:   command,
+				Container: container,
+				Stdin:     true,
+				Stdout:    false,
+				Stderr:    true,
+				TTY:       false,
+			}, parameterCodec)
+
+			exec, err := remotecommand.NewSPDYExecutor(c.ClientConfig, "POST", req.URL())
+			if err != nil {
+				return nil, map[string]string{
+					filepath.Join(copyCollector.ContainerPath, "error"): err.Error(),
+				}
+			}
+
+			output := new(bytes.Buffer)
+			var stderr bytes.Buffer
+			err = exec.Stream(remotecommand.StreamOptions{
+				Stdin:  nil,
+				Stdout: output,
+				Stderr: &stderr,
+				Tty:    false,
+			})
+			files[file] = output.Bytes()
+
+			if err != nil {
+				errors := map[string]string{
+					filepath.Join(copyCollector.ContainerPath, "error"): err.Error(),
+				}
+				if s := output.String(); len(s) > 0 {
+					errors[filepath.Join(copyCollector.ContainerPath, "stdout")] = s
+				}
+				if s := stderr.String(); len(s) > 0 {
+					errors[filepath.Join(copyCollector.ContainerPath, "stderr")] = s
+				}
+				return nil, errors
+			}
+		}
+	}
+	return files, nil
 }
 
 func getCopyErrosFileName(copyCollector *troubleshootv1beta1.Copy) string {
