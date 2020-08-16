3a8126ebf31c224013ee6dc99658a6441d7bd75c
Matias Manavella <matiasm@nimble.la>
2020-08-16 19:47:21 -0300

    tar.gz and .tgz included

--- pkg/collect/redact.go
+++ pkg/collect/redact.go
@@ -3,9 +3,11 @@ package collect
 import (
 	"archive/tar"
 	"bytes"
+	"compress/gzip"
 	"encoding/binary"
 	"io"
 	"path/filepath"
+	"strings"
 
 	troubleshootv1beta1 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta1"
 	"github.com/replicatedhq/troubleshoot/pkg/redact"
@@ -17,11 +19,11 @@ func redactMap(input map[string][]byte, additionalRedactors []*troubleshootv1bet
 		if v == nil {
 			continue
 		}
-		//If the file is a .tar file, it must not be redacted. Instead it is decompressed and each file inside the
+		//If the file is .tar, .tgz or .tar.gz, it must not be redacted. Instead it is decompressed and each file inside the
 		//tar is decompressed, redacted and compressed back into the tar.
-		if filepath.Ext(k) == ".tar" {
+		if filepath.Ext(k) == ".tar" || filepath.Ext(k) == ".tgz" || strings.HasSuffix(k, ".tar.gz") {
 			tarFile := bytes.NewBuffer(v)
-			unRedacted, tarHeaders, err := untarFile(tarFile)
+			unRedacted, tarHeaders, err := untarFile(tarFile, k)
 			if err != nil {
 				return nil, err
 			}
@@ -29,7 +31,7 @@ func redactMap(input map[string][]byte, additionalRedactors []*troubleshootv1bet
 			if err != nil {
 				return nil, err
 			}
-			result[k], err = tarFiles(redacted, tarHeaders)
+			result[k], err = tarFiles(redacted, tarHeaders, k)
 			if err != nil {
 				return nil, err
 			}
@@ -45,9 +47,17 @@ func redactMap(input map[string][]byte, additionalRedactors []*troubleshootv1bet
 	return result, nil
 }
 
-func tarFiles(tarContent map[string][]byte, tarHeaders map[string]*tar.Header) ([]byte, error) {
+func tarFiles(tarContent map[string][]byte, tarHeaders map[string]*tar.Header, filename string) ([]byte, error) {
 	buff := new(bytes.Buffer)
-	tw := tar.NewWriter(buff)
+	var tw *tar.Writer
+	var zw *gzip.Writer
+	if filepath.Ext(filename) == ".tgz" || strings.HasSuffix(filename, ".tar.gz") {
+		zw = gzip.NewWriter(buff)
+		tw = tar.NewWriter(zw)
+		defer zw.Close()
+	} else {
+		tw = tar.NewWriter(buff)
+	}
 	defer tw.Close()
 	for p, f := range tarContent {
 		if tarHeaders[p].FileInfo().IsDir() {
@@ -72,12 +82,29 @@ func tarFiles(tarContent map[string][]byte, tarHeaders map[string]*tar.Header) (
 	if err != nil {
 		return nil, err
 	}
+	if filepath.Ext(filename) == ".tgz" || strings.HasSuffix(filename, ".tar.gz") {
+		err = zw.Close()
+		if err != nil {
+			return nil, err
+		}
+	}
 	return buff.Bytes(), nil
 
 }
 
-func untarFile(tarFile *bytes.Buffer) (map[string][]byte, map[string]*tar.Header, error) {
-	tarReader := tar.NewReader(tarFile)
+func untarFile(tarFile *bytes.Buffer, filename string) (map[string][]byte, map[string]*tar.Header, error) {
+	var tarReader *tar.Reader
+	var zr *gzip.Reader
+	var err error
+	if filepath.Ext(filename) == ".tgz" || strings.HasSuffix(filename, ".tar.gz") {
+		zr, err = gzip.NewReader(tarFile)
+		if err != nil {
+			return nil, nil, err
+		}
+		tarReader = tar.NewReader(zr)
+	} else {
+		tarReader = tar.NewReader(tarFile)
+	}
 	tarHeaders := make(map[string]*tar.Header)
 	tarContent := make(map[string][]byte)
 	for {
