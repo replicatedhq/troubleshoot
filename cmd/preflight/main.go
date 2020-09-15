3f485740711f7c12684699a79bd72b0122b0a9df
Bryant Hagadorn <4451303+blhagadorn@users.noreply.github.com>
2020-09-15 18:34:10 -0400

    Add all auth providers (#266)
    * Update main.go
    
    * Adding generic auth for OIDC

--- cmd/preflight/main.go
+++ cmd/preflight/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/replicatedhq/troubleshoot/cmd/preflight/cli"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 func main() {
