7cb7957e51c294e075ffbc1de9a901df6a23faf6
Matias Manavella <matiasm@nimble.la>
2020-10-06 14:40:46 -0300

    run github actions

--- pkg/analyze/text_analyze_test.go
+++ pkg/analyze/text_analyze_test.go
@@ -333,7 +333,6 @@ func Test_textAnalyze(t *testing.T) {
 			for _, v := range actual {
 				unPointered = append(unPointered, *v)
 			}
-
 			assert.Equal(t, test.expectResult, unPointered)
 		})
 	}
