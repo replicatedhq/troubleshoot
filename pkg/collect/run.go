bd7eaf78068d79df0b0a2cc12b3d754a21ec9bd1
Ferran <ferran.castell@storageos.com>
2020-10-11 19:10:14 +0100

    Run collector: One Pod per node when using selectors
    When node selectors are defined in a run collector, one Pod is created
    per node that satisfies the nodeSelector terms.
    
    In case of needing a Pod to run in one Node specificly, the user must
    define a nodeSelector that is only fulfiled by one node.
    
    Logs from the Pod are gathered in one action, hence a timeout defined in
    the run collector applies for the gathering of data from all Pods

--- pkg/collect/run.go
+++ pkg/collect/run.go
@@ -5,8 +5,10 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
+	"path/filepath"
 	"strings"
 	"time"
+	"fmt"
 
 	"github.com/pkg/errors"
 	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
@@ -24,18 +26,52 @@ func Run(c *Collector, runCollector *troubleshootv1beta2.Run) (map[string][]byte
 		return nil, errors.Wrap(err, "failed to create client from config")
 	}
 
-	pod, err := runPod(ctx, client, runCollector, c.Namespace)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to run pod")
+	// List of Pods that will be created
+	var pods []corev1.Pod
+
+	if runCollector.NodeSelector != nil {
+		// Get all the nodes where a Pod should be running
+		nodes, err := listNodesInSelectors(ctx, client, runCollector.NodeSelector)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to get the list of nodes matching a nodeSelector")
+		}
+
+		for _, node := range nodes {
+			// We create a Pod on every Node that satisfies the NodeSelector
+
+			nodeHostName := node.ObjectMeta.Labels["kubernetes.io/hostname"]
+			nodeSelector := map[string]string{
+				"kubernetes.io/hostname": nodeHostName,
+			}
+
+			podname := runCollector.CollectorName + "-" + nodeHostName
+			pod, err := runPod(ctx, client, runCollector, c.Namespace, podname, nodeSelector)
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to run Pod")
+			}
+
+			pods = append(pods, *pod)
+		}
+	} else {
+		pod, err := runPod(ctx, client, runCollector, c.Namespace, "pod", nil)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to run pod")
+		}
+
+		pods = append(pods, *pod)
 	}
 
 	defer func() {
-		if err := client.CoreV1().Pods(pod.Namespace).Delete(ctx, pod.Name, metav1.DeleteOptions{}); err != nil {
-			logger.Printf("Failed to delete pod %s: %v\n", pod.Name, err)
+		for _, pod := range pods {
+			if err := client.CoreV1().Pods(pod.Namespace).Delete(ctx, pod.Name, metav1.DeleteOptions{}); err != nil {
+				logger.Printf("Failed to delete pod %s: %v\n", pod.Name, err)
+			}
 		}
 	}()
+
 	if runCollector.ImagePullSecret != nil && runCollector.ImagePullSecret.Data != nil {
 		defer func() {
+			pod := pods[0] // The Secret is the same for all Pods, we only need to delete it once
 			for _, k := range pod.Spec.ImagePullSecrets {
 				if err := client.CoreV1().Secrets(pod.Namespace).Delete(ctx, k.Name, metav1.DeleteOptions{}); err != nil {
 					logger.Printf("Failed to delete secret %s: %v\n", k.Name, err)
@@ -43,8 +79,9 @@ func Run(c *Collector, runCollector *troubleshootv1beta2.Run) (map[string][]byte
 			}
 		}()
 	}
+
 	if runCollector.Timeout == "" {
-		return runWithoutTimeout(ctx, c, pod, runCollector)
+		return runWithoutTimeout(ctx, c, pods, runCollector)
 	}
 
 	timeout, err := time.ParseDuration(runCollector.Timeout)
@@ -55,7 +92,7 @@ func Run(c *Collector, runCollector *troubleshootv1beta2.Run) (map[string][]byte
 	errCh := make(chan error, 1)
 	resultCh := make(chan map[string][]byte, 1)
 	go func() {
-		b, err := runWithoutTimeout(ctx, c, pod, runCollector)
+		b, err := runWithoutTimeout(ctx, c, pods, runCollector)
 		if err != nil {
 			errCh <- err
 		} else {
@@ -73,50 +110,68 @@ func Run(c *Collector, runCollector *troubleshootv1beta2.Run) (map[string][]byte
 	}
 }
 
-func runWithoutTimeout(ctx context.Context, c *Collector, pod *corev1.Pod, runCollector *troubleshootv1beta2.Run) (map[string][]byte, error) {
+func runWithoutTimeout(ctx context.Context, c *Collector, pods []corev1.Pod, runCollector *troubleshootv1beta2.Run) (map[string][]byte, error) {
 	client, err := kubernetes.NewForConfig(c.ClientConfig)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed create client from config")
 	}
 
-	for {
-		status, err := client.CoreV1().Pods(pod.Namespace).Get(ctx, pod.Name, metav1.GetOptions{})
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to get pod")
-		}
-		if status.Status.Phase == corev1.PodRunning ||
-			status.Status.Phase == corev1.PodFailed ||
-			status.Status.Phase == corev1.PodSucceeded {
-			break
-		}
-		if status.Status.Phase == corev1.PodPending {
-			for _, v := range status.Status.ContainerStatuses {
-				if v.State.Waiting != nil && v.State.Waiting.Reason == "ImagePullBackOff" {
-					return nil, errors.Errorf("run pod aborted after getting pod status 'ImagePullBackOff'")
-				}
-			}
-		}
-		time.Sleep(time.Second * 1)
-	}
-
+	// Data structure to gather the output of all Pods executed
 	runOutput := map[string][]byte{}
 
-	limits := troubleshootv1beta2.LogLimits{
-		MaxLines: 10000,
-	}
-	podLogs, err := getPodLogs(ctx, client, *pod, runCollector.Name, "", &limits, true)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get pod logs")
-	}
+	podsloop:
+	for _, pod := range pods {
+		for {
+			status, err := client.CoreV1().Pods(pod.Namespace).Get(ctx, pod.Name, metav1.GetOptions{})
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to get pod")
+			}
+			if status.Status.Phase == corev1.PodRunning ||
+				status.Status.Phase == corev1.PodFailed ||
+				status.Status.Phase == corev1.PodSucceeded {
+				break
+			}
+			if status.Status.Phase == corev1.PodPending {
+				for _, condition := range status.Status.Conditions {
+					if condition.Reason == "Unschedulable" {
+						logger.Printf("run pod %s aborted after getting pod status 'Unschedulable'", pod.Name)
 
-	for k, v := range podLogs {
-		runOutput[k] = v
+						// Because of the Pod is in Pending state and unschedulable, we
+						// don't want to gather its logs and proceed with the rest of data
+						// gathering from it.
+						// The Pod can be in that state because the a node is cordon, tainted, etc
+						break podsloop
+					}
+				}
+
+				for _, v := range status.Status.ContainerStatuses {
+					if v.State.Waiting != nil && v.State.Waiting.Reason == "ImagePullBackOff" {
+						return nil, errors.Errorf("run pod aborted after getting pod status 'ImagePullBackOff'")
+					}
+				}
+			}
+			time.Sleep(time.Second * 1)
+		}
+
+		limits := troubleshootv1beta2.LogLimits{
+			MaxLines: 10000,
+		}
+		podLogs, err := getPodLogs(ctx, client, pod, runCollector.Name, "", &limits, true)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to get pod logs")
+		}
+
+		for _, v := range podLogs {
+			// logfile path = collectorName/nodeName-podName.log
+			k := filepath.Join(runCollector.Name, pod.Name+".log")
+			runOutput[k] = v
+		}
 	}
 
 	return runOutput, nil
 }
 
-func runPod(ctx context.Context, client *kubernetes.Clientset, runCollector *troubleshootv1beta2.Run, namespace string) (*corev1.Pod, error) {
+func runPod(ctx context.Context, client *kubernetes.Clientset, runCollector *troubleshootv1beta2.Run, namespace string, podname string, nodeSelector map[string]string) (*corev1.Pod, error) {
 	podLabels := make(map[string]string)
 	podLabels["troubleshoot-role"] = "run-collector"
 
@@ -132,6 +187,10 @@ func runPod(ctx context.Context, client *kubernetes.Clientset, runCollector *tro
 		namespace = "default"
 	}
 
+	if podname == "" {
+		podname = runCollector.CollectorName
+	}
+
 	ips, err := GetNodesPrivateIpAddresses(ctx, client)
 	if err != nil {
 		return nil, err
@@ -139,7 +198,7 @@ func runPod(ctx context.Context, client *kubernetes.Clientset, runCollector *tro
 
 	pod := corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      runCollector.CollectorName,
+			Name:      podname,
 			Namespace: namespace,
 			Labels:    podLabels,
 		},
@@ -151,7 +210,7 @@ func runPod(ctx context.Context, client *kubernetes.Clientset, runCollector *tro
 			RestartPolicy: corev1.RestartPolicyNever,
 			HostNetwork:   runCollector.HostNetwork,
 			HostPID:       runCollector.HostPID,
-			NodeSelector:  runCollector.NodeSelector,
+			NodeSelector:  nodeSelector,
 			Containers: []corev1.Container{
 				{
 					Image:           runCollector.Image,
@@ -183,6 +242,7 @@ func runPod(ctx context.Context, client *kubernetes.Clientset, runCollector *tro
 
 	return created, nil
 }
+
 func createSecret(ctx context.Context, client *kubernetes.Clientset, imagePullSecret *troubleshootv1beta2.ImagePullSecrets, pod *corev1.Pod) error {
 	//In case a new secret needs to be created
 	if imagePullSecret.Data != nil {
@@ -237,4 +297,23 @@ func createSecret(ctx context.Context, client *kubernetes.Clientset, imagePullSe
 		return nil
 	}
 	return errors.Errorf("Secret must at least have a Name")
+}
+
+func listNodesInSelectors(ctx context.Context, client *kubernetes.Clientset, selector map[string]string) ([]corev1.Node, error) {
+	var labels []string
+	for key, value := range selector {
+		labels = append(labels, fmt.Sprintf("%s=%s", key, value))
+	}
+	serializedLabelSelector := strings.Join(labels, ",")
+
+	listOptions := metav1.ListOptions{
+		LabelSelector: serializedLabelSelector,
+	}
+
+	nodes, err := client.CoreV1().Nodes().List(ctx, listOptions)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't get the list of nodes")
+	}
+
+	return nodes.Items, nil
 }
