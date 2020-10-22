56408fab01dfe806f82aa6fd16ad03100e8e2c5d
Matias Manavella <matiasm@nimble.la>
2020-10-22 14:28:35 -0300

    return error when both flags are used

--- cmd/preflight/cli/run.go
+++ cmd/preflight/cli/run.go
@@ -177,7 +177,7 @@ func parseTimeFlags(v *viper.Viper, progressChan chan interface{}, collectors []
 	)
 	if v.GetString("since-time") != "" {
 		if v.GetString("since") != "" {
-			progressChan <- errors.Errorf("Only one of since-time / since may be used. The flag since-time will be used.")
+			return errors.Errorf("at most one of `sinceTime` or `sinceSeconds` may be specified")
 		}
 		sinceTime, err = time.Parse(time.RFC3339, v.GetString("since-time"))
 		if err != nil {
--- cmd/troubleshoot/cli/run.go
+++ cmd/troubleshoot/cli/run.go
@@ -694,7 +694,7 @@ func parseTimeFlags(v *viper.Viper, progressChan chan interface{}, collectors *c
 	)
 	if v.GetString("since-time") != "" {
 		if v.GetString("since") != "" {
-			progressChan <- errors.Errorf("Only one of since-time / since may be used. The flag since-time will be used.")
+			return errors.Errorf("Only one of since-time / since may be used. The flag since-time will be used.")
 		}
 		sinceTime, err = time.Parse(time.RFC3339, v.GetString("since-time"))
 		if err != nil {
