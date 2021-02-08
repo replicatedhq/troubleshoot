b5a68be286ef907cb8cc4d06e445f84a47923b75
Andrew Reed <andrew@replicated.com>
2021-02-08 20:57:03 +0000

    Review changes

--- pkg/analyze/host_cpu_test.go
+++ pkg/analyze/host_cpu_test.go
@@ -71,3 +71,80 @@ func Test_doCompareHostCPU(t *testing.T) {
 		})
 	}
 }
+
+func Test_compareHostCPUConditionalToActual(t *testing.T) {
+	tests := []struct {
+		name          string
+		when          string
+		logicalCount  int
+		physicalCount int
+		expected      bool
+	}{
+		{
+			name:          "physical > 4, when physical is 8",
+			when:          "physical > 4",
+			logicalCount:  0,
+			physicalCount: 8,
+			expected:      true,
+		},
+		{
+			name:          "physical > 4, when physical is 4",
+			when:          "physical > 4",
+			logicalCount:  0,
+			physicalCount: 4,
+			expected:      false,
+		},
+		{
+			name:          "physical > 4, when physical is 3, logical is 6",
+			when:          "physical > 4",
+			logicalCount:  6,
+			physicalCount: 3,
+			expected:      false,
+		},
+		{
+			name:          "logical > 4, when physical is 4, logical is 8",
+			when:          "logical > 4",
+			logicalCount:  8,
+			physicalCount: 4,
+			expected:      true,
+		},
+		{
+			name:          ">= 4, when physical is 2, logical is 4",
+			when:          ">= 4",
+			logicalCount:  4,
+			physicalCount: 2,
+			expected:      true,
+		},
+		{
+			name:          "count < 4, when physical is 2, logical is 4",
+			when:          "count < 4",
+			logicalCount:  4,
+			physicalCount: 2,
+			expected:      false,
+		},
+		{
+			name:          "count <= 4, when physical is 2, logical is 4",
+			when:          "count <= 4",
+			logicalCount:  4,
+			physicalCount: 2,
+			expected:      true,
+		},
+		{
+			name:          "== 4, physical is 4, logical is 4",
+			when:          "== 4",
+			logicalCount:  4,
+			physicalCount: 4,
+			expected:      true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := require.New(t)
+
+			actual, err := compareHostCPUConditionalToActual(test.when, test.logicalCount, test.physicalCount)
+			req.NoError(err)
+
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
