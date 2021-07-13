564b05369e0c4280aa01998c1ddc6aaa5ee82ec6
Kyle Sorensen <kyles@replicated.com>
2021-07-13 20:21:51 +0000

    test failing scenario

--- pkg/collect/host_network.go
+++ pkg/collect/host_network.go
@@ -31,16 +31,6 @@ type NetworkStatusResult struct {
 	Message string        `json:"message"`
 }
 
-func isValidIPCandidate(address string) bool {
-	validChars := "0123456789."
-	for i := range address {
-		if !strings.Contains(validChars, string(address[i])) {
-			return false
-		}
-	}
-	return true
-}
-
 func isValidLoadBalancerAddress(address string) bool {
 	splitString := strings.Split(address, ":")
 
@@ -63,14 +53,10 @@ func isValidLoadBalancerAddress(address string) bool {
 		return false
 	}
 
-	// Checking if it's all numbers and .
-	if isValidIPCandidate(hostAddress) {
+	// Check for isValidIP
 
-		// Check for isValidIP
-
-		test := validation.IsValidIP(hostAddress)
-		return len(test) == 0
-
+	if len(validation.IsValidIP(hostAddress)) == 0 {
+		return true
 	}
 
 	errs := validation.IsQualifiedName(hostAddress)
