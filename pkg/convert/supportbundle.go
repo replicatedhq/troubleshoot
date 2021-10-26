8ac5fc528b60184bb4ee38bb85f387cd3d7406b8
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2021-10-26 22:09:54 +0000

    insight involved object

--- pkg/convert/supportbundle.go
+++ pkg/convert/supportbundle.go
@@ -7,6 +7,7 @@ import (
 
 	multierror "github.com/hashicorp/go-multierror"
 	analyze "github.com/replicatedhq/troubleshoot/pkg/analyze"
+	corev1 "k8s.io/api/core/v1"
 )
 
 type Meta struct {
@@ -34,11 +35,12 @@ type Insight struct {
 type Result struct {
 	Meta `json:",inline" yaml:",inline" hcl:",inline"`
 
-	Insight      *Insight               `json:"insight" yaml:"insight" hcl:"insight"`
-	Severity     Severity               `json:"severity" yaml:"severity" hcl:"severity"`
-	AnalyzerSpec string                 `json:"analyzerSpec" yaml:"analyzerSpec" hcl:"analyzerSpec"`
-	Variables    map[string]interface{} `json:"variables,omitempty" yaml:"variables,omitempty" hcl:"variables,omitempty"`
-	Error        string                 `json:"error,omitempty" yaml:"error,omitempty" hcl:"error,omitempty"`
+	Insight        *Insight                `json:"insight" yaml:"insight" hcl:"insight"`
+	Severity       Severity                `json:"severity" yaml:"severity" hcl:"severity"`
+	AnalyzerSpec   string                  `json:"analyzerSpec" yaml:"analyzerSpec" hcl:"analyzerSpec"`
+	Variables      map[string]interface{}  `json:"variables,omitempty" yaml:"variables,omitempty" hcl:"variables,omitempty"`
+	Error          string                  `json:"error,omitempty" yaml:"error,omitempty" hcl:"error,omitempty"`
+	InvolvedObject *corev1.ObjectReference `json:"involvedObject,omitempty" yaml:"involvedObject,omitempty" hcl:"involvedObject,omitempty"`
 }
 
 func (m *Insight) Render(data interface{}) (*Insight, error) {
@@ -88,8 +90,9 @@ func FromAnalyzerResult(input []*analyze.AnalyzeResult) []*Result {
 				Primary: i.Title,
 				Detail:  i.Message,
 			},
-			AnalyzerSpec: "",
-			Variables:    map[string]interface{}{},
+			AnalyzerSpec:   "",
+			Variables:      map[string]interface{}{},
+			InvolvedObject: i.InvolvedObject,
 		}
 		if i.IsFail {
 			r.Severity = SeverityError
