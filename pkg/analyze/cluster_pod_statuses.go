e100e7c4788b2bb6060ba7b931baa401b9fa7fb7
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2021-10-28 09:21:14 -0700

    get container logs for unhealthy pods (#469)
    * get container logs for unhealthy pods
    
    Co-authored-by: divolgin <dmitriy@replicated.com>
    Co-authored-by: divolgin <divolgin@users.noreply.github.com>

--- pkg/analyze/cluster_pod_statuses.go
+++ pkg/analyze/cluster_pod_statuses.go
@@ -42,8 +42,6 @@ func clusterPodStatuses(analyzer *troubleshootv1beta2.ClusterPodStatuses, getChi
 	allResults := []*AnalyzeResult{}
 
 	for _, pod := range pods {
-		podResults := []*AnalyzeResult{}
-
 		if pod.Status.Reason == "" {
 			pod.Status.Reason = k8sutil.GetPodStatusReason(&pod)
 		}
@@ -78,12 +76,23 @@ func clusterPodStatuses(analyzer *troubleshootv1beta2.ClusterPodStatuses, getChi
 				continue
 			}
 
+			operator := parts[0]
+			reason := parts[1]
 			match := false
-			switch parts[0] {
+
+			switch operator {
 			case "=", "==", "===":
-				match = parts[1] == string(pod.Status.Phase) || parts[1] == string(pod.Status.Reason)
+				if reason == "Healthy" {
+					match = !k8sutil.IsPodUnhealthy(&pod)
+				} else {
+					match = reason == string(pod.Status.Phase) || reason == string(pod.Status.Reason)
+				}
 			case "!=", "!==":
-				match = parts[1] != string(pod.Status.Phase) && parts[1] != string(pod.Status.Reason)
+				if reason == "Healthy" {
+					match = k8sutil.IsPodUnhealthy(&pod)
+				} else {
+					match = reason != string(pod.Status.Phase) && reason != string(pod.Status.Reason)
+				}
 			}
 
 			if !match {
@@ -132,10 +141,9 @@ func clusterPodStatuses(analyzer *troubleshootv1beta2.ClusterPodStatuses, getChi
 			}
 			r.Message = m.String()
 
-			podResults = append(podResults, &r)
+			allResults = append(allResults, &r)
+			break
 		}
-
-		allResults = append(allResults, podResults...)
 	}
 
 	return allResults, nil
