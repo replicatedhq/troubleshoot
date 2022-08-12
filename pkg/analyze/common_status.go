c3773b7f265cf8a6a62b3ee0acd29367bc2f923d
Dan Jones <danj@replicated.com>
2022-08-12 09:47:46 +0100

    %s -> %q
    Signed-off-by: Dan Jones <danj@replicated.com>

--- pkg/analyze/common_status.go
+++ pkg/analyze/common_status.go
@@ -59,7 +59,7 @@ func commonStatus(outcomes []*troubleshootv1beta2.Outcome, name string, iconKey 
 
 			if exists == false  && outcome.Warn.When != "absent" {
 				result.IsFail = true
-				result.Message = fmt.Sprintf("The %s %s was not found", resourceType, name)
+				result.Message = fmt.Sprintf("The %s %q was not found", resourceType, name)
 				result.URI = outcome.Fail.URI
 				return result, nil
 			}
@@ -95,7 +95,7 @@ func commonStatus(outcomes []*troubleshootv1beta2.Outcome, name string, iconKey 
 
 			if exists == false  && outcome.Pass.When != "absent" {
 				result.IsFail = true
-				result.Message = fmt.Sprintf("The %s %s was not found", resourceType, name)
+				result.Message = fmt.Sprintf("The %s %q was not found", resourceType, name)
 				result.URI = outcome.Fail.URI
 				return result, nil
 			}
