645dae6c81ba72a3151ecb3094fbb8f1f7d676f9
Edgar Lanting <edgarlanting@users.noreply.github.com>
2022-10-05 12:52:52 +0200

    Update root.go
    Rewrite and change flag name from `follow-uri` to `no-uri`

--- cmd/troubleshoot/cli/root.go
+++ cmd/troubleshoot/cli/root.go
@@ -60,7 +60,7 @@ from a server that can be used to assist when troubleshooting a Kubernetes clust
 
 	// `follow-uri` references the `followURI` functionality where we can use an upstream spec when creating a support bundle
 	// This flag makes sure we can also disable this and fall back to the default spec.
-	cmd.Flags().Bool("follow-uri", true, "Forces the support bundle to revert back to the default and not use a URI for a spec when set to `false`")
+	cmd.Flags().Bool("no-uri", false, "Forces the support bundle to revert back to the default and not use a URI for a spec when used.`")
 
 	viper.BindPFlags(cmd.Flags())
 
