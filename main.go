df1dfa6f0682fc5ac751727676733aeac3ba51b7
Evans Mungai <evans@replicated.com>
2022-10-17 13:12:09 +0100

    Use upstream image, its compiled with TLS by default

--- main.go
+++ main.go
@@ -2,6 +2,11 @@
 // A very simple application for making a TLS connection to a redis server
 package main
 
+// TODO: Remove me before merging PR. This is for testing purposes only
+
+// Connect to redis after starting it using
+// docker run -it --rm -v $(pwd)/tls:/redis/tls -p6379:6379 -p6380:6380 redis redis-server --requirepass replicated --tls-port 6380 --port 6379 --tls-cert-file /redis/tls/server.pem --tls-key-file /redis/tls/server-key.pem --tls-ca-cert-file /redis/tls/ca.pem --loglevel debug
+
 import (
 	"crypto/tls"
 	"crypto/x509"
@@ -76,13 +81,29 @@ func main() {
 	caPEM, err := os.ReadFile(caFile)
 	checkErr(err)
 
-	rdb := connectTLS(certPEMBlock, keyPEMBlock, caPEM, serverName)
+	rdbTLS := connectTLS(certPEMBlock, keyPEMBlock, caPEM, serverName)
+	defer rdbTLS.Close()
+
+	err = rdbTLS.Set("key", "My TLS Value for Replicated", 0).Err()
+    if err != nil {
+        panic(err)
+    }
+
+    val, err := rdbTLS.Get("key").Result()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("key:", val)
+
+	rdb := connect()
+	defer rdb.Close()
+
 	err = rdb.Set("key", "My Value for Replicated", 0).Err()
     if err != nil {
         panic(err)
     }
 
-    val, err := rdb.Get("key").Result()
+    val, err = rdb.Get("key").Result()
     if err != nil {
         panic(err)
     }
