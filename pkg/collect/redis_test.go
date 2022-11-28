2dfdae9fa258aca739d9a45510f76a193268c107
Evans Mungai <evans@replicated.com>
2022-11-28 15:14:51 +0000

    Add Redis TLS connection test

--- pkg/collect/redis_test.go
+++ pkg/collect/redis_test.go
@@ -6,6 +6,7 @@ import (
 	v1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	testclient "k8s.io/client-go/kubernetes/fake"
 )
 
 func Test_extractServerVersion(t *testing.T) {
@@ -79,4 +80,29 @@ func TestCollectRedis_createPlainTextClient(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCollectRedis_createTLSClient(t *testing.T) {
+	k8sClient := testclient.NewSimpleClientset()
+
+	c := &CollectRedis{
+		Client: k8sClient,
+		Collector: &v1beta2.Database{
+			URI: "redis://localhost:6379",
+			TLS: &v1beta2.TLSParams{
+				CACert:     getTestFixture(t, "db/ca.pem"),
+				ClientCert: getTestFixture(t, "db/client.pem"),
+				ClientKey:  getTestFixture(t, "db/client-key.pem"),
+			},
+		},
+	}
+
+	client, err := c.createClient()
+	assert.NoError(t, err)
+	assert.NotNil(t, client)
+	opt := client.Options()
+	assert.Equal(t, opt.Addr, "localhost:6379")
+	assert.NotNil(t, opt.TLSConfig.Certificates)
+	assert.NotNil(t, opt.TLSConfig.RootCAs)
+	assert.False(t, opt.TLSConfig.InsecureSkipVerify)
 }
