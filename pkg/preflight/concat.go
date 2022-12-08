ede88b5aeae7f422e4d0867dcae5555a58a0cc4c
Diamon Wiggins <diamonw757@gmail.com>
2022-12-08 16:03:57 -0400

    add concat.go

---
+++ pkg/preflight/concat.go
@@ -0,0 +1,22 @@
+package preflight
+
+import (
+	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
+)
+
+func ConcatPreflightSpec(target *troubleshootv1beta2.Preflight, source *troubleshootv1beta2.Preflight) *troubleshootv1beta2.Preflight {
+	newSpec := target.DeepCopy()
+	newSpec.Spec.Collectors = append(target.Spec.Collectors, source.Spec.Collectors...)
+	newSpec.Spec.RemoteCollectors = append(target.Spec.RemoteCollectors, source.Spec.RemoteCollectors...)
+	//newSpec.Spec.UploadResultsTo = append(target.Spec.UploadResultsTo, source.Spec.UploadResultsTo...)
+	newSpec.Spec.Analyzers = append(target.Spec.Analyzers, source.Spec.Analyzers...)
+	return newSpec
+}
+
+func ConcatHostPreflightSpec(target *troubleshootv1beta2.HostPreflight, source *troubleshootv1beta2.HostPreflight) *troubleshootv1beta2.HostPreflight {
+	newSpec := target.DeepCopy()
+	newSpec.Spec.Collectors = append(target.Spec.Collectors, source.Spec.Collectors...)
+	newSpec.Spec.RemoteCollectors = append(target.Spec.RemoteCollectors, source.Spec.RemoteCollectors...)
+	newSpec.Spec.Analyzers = append(target.Spec.Analyzers, source.Spec.Analyzers...)
+	return newSpec
+}
