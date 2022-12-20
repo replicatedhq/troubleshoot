8c5758202be916ebca65cf078a46acaaa1e7073b
Dexter Yan <yanshaocong@gmail.com>
2022-12-21 10:25:09 +1300

    feat(collect): add troubleshoot client constants

---
+++ pkg/constants/constants.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	DEFAULT_CLIENT_QPS        = 100
+	DEFAULT_CLIENT_BURST      = 100
+	DEFAULT_CLIENT_USER_AGENT = "ReplicatedTroubleshoot"
+)
--- pkg/supportbundle/collect.go
+++ pkg/supportbundle/collect.go
@@ -13,6 +13,7 @@ import (
 	analyze "github.com/replicatedhq/troubleshoot/pkg/analyze"
 	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
 	"github.com/replicatedhq/troubleshoot/pkg/collect"
+	"github.com/replicatedhq/troubleshoot/pkg/constants"
 	"github.com/replicatedhq/troubleshoot/pkg/convert"
 	"github.com/replicatedhq/troubleshoot/pkg/version"
 	"gopkg.in/yaml.v2"
@@ -77,9 +78,9 @@ func runCollectors(collectors []*troubleshootv1beta2.Collect, additionalRedactor
 	collectSpecs = collect.EnsureCollectorInList(collectSpecs, troubleshootv1beta2.Collect{ClusterResources: &troubleshootv1beta2.ClusterResources{}})
 	collectSpecs = collect.EnsureClusterResourcesFirst(collectSpecs)
 
-	opts.KubernetesRestConfig.QPS = 100
-	opts.KubernetesRestConfig.Burst = 100
-	opts.KubernetesRestConfig.UserAgent = fmt.Sprintf("ReplicatedTroubleshoot/%s", version.Version())
+	opts.KubernetesRestConfig.QPS = constants.DEFAULT_CLIENT_QPS
+	opts.KubernetesRestConfig.Burst = constants.DEFAULT_CLIENT_BURST
+	opts.KubernetesRestConfig.UserAgent = fmt.Sprintf("%s/%s", constants.DEFAULT_CLIENT_USER_AGENT, version.Version())
 
 	k8sClient, err := kubernetes.NewForConfig(opts.KubernetesRestConfig)
 	if err != nil {
