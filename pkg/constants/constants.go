f7ab65cfd5e142c2fbe646e4311d4618453c0cd3
Dexter Yan <yanshaocong@gmail.com>
2022-12-21 12:02:00 +1300

    feat(collect): add comments for troubleshoot client constants

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -1,7 +1,10 @@
 package constants
 
 const (
-	DEFAULT_CLIENT_QPS        = 100
-	DEFAULT_CLIENT_BURST      = 100
+	// DEFAULT_CLIENT_QPS indicates the maximum QPS from troubleshoot client.
+	DEFAULT_CLIENT_QPS = 100
+	// DEFAULT_CLIENT_QPS is maximum burst for throttle.
+	DEFAULT_CLIENT_BURST = 100
+	// DEFAULT_CLIENT_USER_AGENT is an field that specifies the caller of troubleshoot request.
 	DEFAULT_CLIENT_USER_AGENT = "ReplicatedTroubleshoot"
 )
