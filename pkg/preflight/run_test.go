0e3b88e3ff072b1b5c8bae544f9995f8c8ce463d
Nathan Sullivan <nathans@replicated.com>
2023-01-10 20:12:56 +1000

    preflight tests: use the testify libraries, restructure code to be formatted more like other tests in this project

--- pkg/preflight/run_test.go
+++ pkg/preflight/run_test.go
@@ -7,12 +7,13 @@
 package preflight
 
 import (
-	"errors"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/replicatedhq/troubleshoot/internal/testutils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	/*
 		See TODOLATER below
 
@@ -27,14 +28,14 @@ func TestRunPreflights(t *testing.T) {
 	// A very simple preflight spec (local file)
 	preflightFile := filepath.Join(testutils.FileDir(), "../../testdata/preflightspec/troubleshoot_v1beta2_preflight_gotest.yaml")
 
-	expectedOutputContentHuman := `
+	wantOutputContentHuman := `
    --- PASS Compare JSON Example
       --- The collected data matches the value.
 --- PASS   go-test-preflight
 PASS
 `
 
-	expectedOutputContentJson := `{
+	wantOutputContentJson := `{
   "pass": [
     {
       "title": "Compare JSON Example",
@@ -44,70 +45,77 @@ PASS
 }
 `
 
-	expectedOutputContentYaml := `pass:
+	wantOutputContentYaml := `pass:
 - title: Compare JSON Example
   message: The collected data matches the value.
 
 `
 
 	tests := []struct {
+		name        string
 		interactive bool
 		output      string
 		format      string
 		args        []string
 		//
-		expectedError error
+		wantErr bool
 		// May be in stdout or file, depending on above value
-		expectedOutputContent string
+		wantOutputContent string
 	}{
 		// TODOLATER: test interactive true as well
 		{
-			interactive:           false,
-			output:                "",
-			format:                "human",
-			args:                  []string{preflightFile},
-			expectedError:         nil,
-			expectedOutputContent: expectedOutputContentHuman,
+			name:              "noninteractive-stdout-human",
+			interactive:       false,
+			output:            "",
+			format:            "human",
+			args:              []string{preflightFile},
+			wantErr:           false,
+			wantOutputContent: wantOutputContentHuman,
 		},
 		{
-			interactive:           false,
-			output:                "",
-			format:                "json",
-			args:                  []string{preflightFile},
-			expectedError:         nil,
-			expectedOutputContent: expectedOutputContentJson,
+			name:              "noninteractive-stdout-json",
+			interactive:       false,
+			output:            "",
+			format:            "json",
+			args:              []string{preflightFile},
+			wantErr:           false,
+			wantOutputContent: wantOutputContentJson,
 		},
 		{
-			interactive:           false,
-			output:                "",
-			format:                "yaml",
-			args:                  []string{preflightFile},
-			expectedError:         nil,
-			expectedOutputContent: expectedOutputContentYaml,
+			name:              "noninteractive-stdout-yaml",
+			interactive:       false,
+			output:            "",
+			format:            "yaml",
+			args:              []string{preflightFile},
+			wantErr:           false,
+			wantOutputContent: wantOutputContentYaml,
 		},
 		{
-			interactive:           false,
-			output:                testutils.TempFilename("preflight_out_test_"),
-			format:                "human",
-			args:                  []string{preflightFile},
-			expectedError:         nil,
-			expectedOutputContent: expectedOutputContentHuman,
+			name:              "noninteractive-tofile-human",
+			interactive:       false,
+			output:            testutils.TempFilename("preflight_out_test_"),
+			format:            "human",
+			args:              []string{preflightFile},
+			wantErr:           false,
+			wantOutputContent: wantOutputContentHuman,
 		},
 		{
-			interactive:           false,
-			output:                testutils.TempFilename("preflight_out_test_"),
-			format:                "json",
-			args:                  []string{preflightFile},
-			expectedError:         nil,
-			expectedOutputContent: expectedOutputContentJson,
+			name:              "noninteractive-tofile-json",
+			interactive:       false,
+			output:            testutils.TempFilename("preflight_out_test_"),
+			format:            "json",
+			args:              []string{preflightFile},
+			wantErr:           false,
+			wantOutputContent: wantOutputContentJson,
 		},
 		{
-			interactive:           false,
-			output:                testutils.TempFilename("preflight_out_test_"),
-			format:                "yaml",
-			args:                  []string{preflightFile},
-			expectedError:         nil,
-			expectedOutputContent: expectedOutputContentYaml,
+			name:              "noninteractive-tofile-yaml",
+			interactive:       false,
+			output:            testutils.TempFilename("preflight_out_test_"),
+			format:            "yaml",
+			args:              []string{preflightFile},
+			wantErr:           false,
+			wantOutputContent: wantOutputContentYaml,
 		},
 	}
 
@@ -148,79 +156,61 @@ PASS
 	*/
 
 	for _, tt := range tests {
-		// Capture stdout/stderr along the way
-		rOut, wOut, err := os.Pipe()
-		if err != nil {
-			t.Fatal(err)
-		}
-		rErr, wErr, err := os.Pipe()
-		if err != nil {
-			t.Fatal(err)
-		}
-		// Redirect stdout/err to the pipes temporarily
-		origStdout := os.Stdout
-		os.Stdout = wOut
-		origStderr := os.Stderr
-		os.Stderr = wErr
+		t.Run(tt.name, func(t *testing.T) {
+			// Capture stdout/stderr along the way
+			rOut, wOut, err := os.Pipe()
+			require.Nil(t, err)
+			rErr, wErr, err := os.Pipe()
+			require.Nil(t, err)
+			// Redirect stdout/err to the pipes temporarily
+			origStdout := os.Stdout
+			os.Stdout = wOut
+			origStderr := os.Stderr
+			os.Stderr = wErr
 
-		tErr := RunPreflights(tt.interactive, tt.output, tt.format, tt.args)
+			tErr := RunPreflights(tt.interactive, tt.output, tt.format, tt.args)
 
-		// Stop redirection of stdout/stderr
-		bufOut := make([]byte, 1024)
-		nOut, err := rOut.Read(bufOut)
-		if err != nil {
-			t.Fatal(err)
-		}
-		bufErr := make([]byte, 1024)
-		// nErr
-		_, err = rErr.Read(bufErr)
-		if err != nil {
-			t.Fatal(err)
-		}
-		os.Stdout = origStdout
-		os.Stderr = origStderr
+			// Stop redirection of stdout/stderr
+			bufOut := make([]byte, 1024)
+			nOut, err := rOut.Read(bufOut)
+			require.Nil(t, err)
+			bufErr := make([]byte, 1024)
+			// nErr
+			_, err = rErr.Read(bufErr)
+			require.Nil(t, err)
+			os.Stdout = origStdout
+			os.Stderr = origStderr
 
-		if tErr != tt.expectedError {
-			t.Error(err)
-		}
-
-		useBufOut := string(bufOut[:nOut])
-		//useBufErr := string(bufErr[:nErr])
-		//fmt.Printf("stdout: %+v\n", useBufOut)
-		//fmt.Printf("stderr: %+v\n", useBufErr)
-
-		if tt.output != "" {
-			// Output file is expected, make sure it exists
-			if _, err := os.Stat(tt.output); errors.Is(err, os.ErrNotExist) {
-				t.Errorf("interactive: %t format %s :: Output file %s was expected, does not exist\n", tt.interactive, tt.format, tt.output)
+			if tt.wantErr {
+				assert.Error(t, tErr)
 			} else {
+				require.NoError(t, tErr)
+			}
+
+			useBufOut := string(bufOut[:nOut])
+			//useBufErr := string(bufErr[:nErr])
+			//fmt.Printf("stdout: %+v\n", useBufOut)
+			//fmt.Printf("stderr: %+v\n", useBufErr)
+
+			if tt.output != "" {
+				// Output file is expected, make sure it exists
+				assert.FileExists(t, tt.output)
 				// If it exists, check contents of output file against expected
 				readOutputFile, err := os.ReadFile(tt.output)
-				if err != nil {
-					t.Error(err)
-				}
-				if string(readOutputFile) != tt.expectedOutputContent {
-					t.Errorf("interactive: %t format %s :: Output in file does not match expected. Expected length: %d, stdout buffer: %d\n", tt.interactive, tt.format, len(tt.expectedOutputContent), len(readOutputFile))
-				}
+				require.NoError(t, err)
+				assert.Equal(t, string(readOutputFile), tt.wantOutputContent)
+			} else {
+				// Expected no output file, make sure it doesn't exist
+				assert.NoFileExists(t, tt.output)
+				// Check stdout against expected output
+				assert.Equal(t, useBufOut, tt.wantOutputContent)
 			}
-		} else {
-			// Expected no output file, make sure it doesn't exist
+
+			// Remove the (temp) output file if it exists
 			if _, err := os.Stat(tt.output); err == nil {
-				t.Errorf("interactive: %t format %s :: Output file %s was not expected, but one was found\n", tt.interactive, tt.format, tt.output)
+				err = os.Remove(tt.output)
+				require.NoError(t, err)
 			}
-
-			// Check stdout against expected output
-			if useBufOut != tt.expectedOutputContent {
-				t.Errorf("interactive: %t format %s :: Output to stdout does not match expected. Expected length: %d, stdout buffer length: %d\n\nexpected: ''%s''\n\ngot: ''%s''\n", tt.interactive, tt.format, len(tt.expectedOutputContent), len(useBufOut), tt.expectedOutputContent, useBufOut)
-			}
-		}
-
-		// Remove the (temp) output file if it exists
-		if _, err := os.Stat(tt.output); err == nil {
-			err = os.Remove(tt.output)
-			if err != nil {
-				t.Error(err)
-			}
-		}
+		})
 	}
 }
