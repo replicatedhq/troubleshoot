8530b20eda2b0850701192d68ee072c0b61470f9
Nathan Sullivan <nathans@replicated.com>
2023-02-04 12:07:31 +1000

    working parsing of /proc/net/route to obtain ipv4 routes (without the netlink package or iproute2 as a host package dependency)

--- pkg/collect/host_subnetavailable.go
+++ pkg/collect/host_subnetavailable.go
@@ -2,8 +2,13 @@ package collect
 
 import (
 	"bytes"
+	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"net"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/pkg/errors"
 	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
@@ -23,8 +28,6 @@ func (c *CollectHostSubnetAvailable) IsExcluded() (bool, error) {
 }
 
 func (c *CollectHostSubnetAvailable) Collect(progressChan chan<- interface{}) (map[string][]byte, error) {
-
-	// TODO: run `ip route` and get the output (if available)
 
 	//debug.Printf("Routes: %+v\n", routes)
 
@@ -47,4 +50,72 @@ func (c *CollectHostSubnetAvailable) Collect(progressChan chan<- interface{}) (m
 	return map[string][]byte{
 		name: b,
 	}, nil
+}
+
+type systemRoutes []systemRoute
+
+type systemRoute struct {
+	Iface   string
+	DestNet net.IPNet
+	Gateway net.IP
+	Metric  uint32
+}
+
+// Parses the output of /proc/net/route into something useful
+// This only deals with IPv4 - another file /proc/net/ipv6_route deals with IPv6 (not implemented here)
+func parseProcNetRoute(input string) (systemRoutes, error) {
+	routes := systemRoutes{}
+	for _, line := range strings.Split(input, "\n") {
+		if line[0:5] == "Iface" {
+			continue
+		}
+
+		splitLine := strings.Split(strings.TrimSpace(line), "\t")
+		if len(splitLine) != 11 {
+			return []systemRoute{}, errors.Errorf("invalid /proc/net/route line '%s', expected 11 columns got %d", line, len(splitLine))
+		}
+
+		dest, err := hex.DecodeString(strings.TrimSpace(splitLine[1]))
+		if err != nil {
+			return []systemRoute{}, errors.Wrapf(err, "cannot parse dest column (index 1) for /proc/net/route line '%s'", line)
+		}
+		destStr := fmt.Sprintf("%d.%d.%d.%d", dest[3], dest[2], dest[1], dest[0])
+
+		gw, err := hex.DecodeString(strings.TrimSpace(splitLine[2]))
+		if err != nil {
+			return []systemRoute{}, errors.Wrapf(err, "cannot parse gateway column (index 2) for /proc/net/route line '%s'", line)
+		}
+		gwStr := fmt.Sprintf("%d.%d.%d.%d", gw[3], gw[2], gw[1], gw[0])
+
+		mask, err := hex.DecodeString(strings.TrimSpace(splitLine[7]))
+		if err != nil {
+			return []systemRoute{}, errors.Wrapf(err, "cannot parse mask column (index 7) for /proc/net/route line '%s'", line)
+		}
+		maskStr := fmt.Sprintf("%d.%d.%d.%d", mask[3], mask[2], mask[1], mask[0])
+		maskBytes := []byte{}
+		for _, v := range strings.Split(maskStr, ".") {
+			maskByte, err := strconv.Atoi(v)
+			if err != nil {
+				return []systemRoute{}, errors.Wrapf(err, "cannot convert mask octet '%s' to byte", v)
+			}
+			maskBytes = append(maskBytes, byte(maskByte))
+		}
+
+		metric, err := strconv.Atoi(strings.TrimSpace(splitLine[6]))
+		if err != nil {
+			return []systemRoute{}, errors.Wrapf(err, "cannot parse metric column (index 6) for /proc/net/route line '%s'", line)
+		}
+
+		routes = append(routes, systemRoute{
+			Iface: strings.TrimSpace(splitLine[0]),
+			DestNet: net.IPNet{
+				IP:   net.ParseIP(destStr),
+				Mask: net.IPv4Mask(maskBytes[0], maskBytes[1], maskBytes[2], maskBytes[3]),
+			},
+			Gateway: net.ParseIP(gwStr),
+			Metric:  uint32(metric),
+		})
+	}
+
+	return routes, nil
 }
--- pkg/collect/host_subnetavailable_test.go
+++ pkg/collect/host_subnetavailable_test.go
@@ -1,13 +1,14 @@
 package collect
 
 import (
-	"os"
+	"net"
 	"testing"
 
-	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+/*
 func TestCollectHostSubnetAvailable_Collect(t *testing.T) {
 	type fields struct {
 		hostCollector *troubleshootv1beta2.SubnetAvailable
@@ -37,4 +38,76 @@ func TestCollectHostSubnetAvailable_Collect(t *testing.T) {
 			}
 		})
 	}
+}
+*/
+
+func TestParseProcNetRoute(t *testing.T) {
+	input := `Iface	Destination	Gateway 	Flags	RefCnt	Use	Metric	Mask		MTU	Window	IRTT
+eth1	00000000	016BA8C0	0003	0	0	200	00000000	0	0	0
+eth0	00000000	0205A8C0	0003	0	0	202	00000000	0	0	0
+docker0	000011AC	00000000	0001	0	0	0	0000FFFF	0	0	0
+eth0	0005A8C0	00000000	0001	0	0	0	00FFFFFF	0	0	0
+eth1	006BA8C0	00000000	0001	0	0	0	00FFFFFF	0	0	0`
+
+	/*
+	   Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
+	   0.0.0.0         192.168.107.1   0.0.0.0         UG    200    0        0 eth1
+	   0.0.0.0         192.168.5.2     0.0.0.0         UG    202    0        0 eth0
+	   172.17.0.0      0.0.0.0         255.255.0.0     U     0      0        0 docker0
+	   192.168.5.0     0.0.0.0         255.255.255.0   U     0      0        0 eth0
+	   192.168.107.0   0.0.0.0         255.255.255.0   U     0      0        0 eth1
+	*/
+
+	expected := systemRoutes{
+		systemRoute{
+			Iface: "eth1",
+			DestNet: net.IPNet{
+				IP:   net.IPv4(0, 0, 0, 0),
+				Mask: net.CIDRMask(0, 32),
+			},
+			Gateway: net.IPv4(192, 168, 107, 1),
+			Metric:  uint32(200),
+		},
+		systemRoute{
+			Iface: "eth0",
+			DestNet: net.IPNet{
+				IP:   net.IPv4(0, 0, 0, 0),
+				Mask: net.CIDRMask(0, 32),
+			},
+			Gateway: net.IPv4(192, 168, 5, 2),
+			Metric:  uint32(202),
+		},
+		systemRoute{
+			Iface: "docker0",
+			DestNet: net.IPNet{
+				IP:   net.IPv4(172, 17, 0, 0),
+				Mask: net.CIDRMask(16, 32),
+			},
+			Gateway: net.IPv4(0, 0, 0, 0),
+			Metric:  uint32(0),
+		},
+		systemRoute{
+			Iface: "eth0",
+			DestNet: net.IPNet{
+				IP:   net.IPv4(192, 168, 5, 0),
+				Mask: net.CIDRMask(24, 32),
+			},
+			Gateway: net.IPv4(0, 0, 0, 0),
+			Metric:  uint32(0),
+		},
+		systemRoute{
+			Iface: "eth1",
+			DestNet: net.IPNet{
+				IP:   net.IPv4(192, 168, 107, 0),
+				Mask: net.CIDRMask(24, 32),
+			},
+			Gateway: net.IPv4(0, 0, 0, 0),
+			Metric:  uint32(0),
+		},
+	}
+
+	result, err := parseProcNetRoute(input)
+
+	require.NoError(t, err)
+	assert.Equal(t, expected, result)
 }
