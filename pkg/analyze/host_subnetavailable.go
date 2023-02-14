be1591c8c6805116c1503ad4d746ded4eb35e01f
Nathan Sullivan <nathans@replicated.com>
2023-02-15 06:17:34 +1000

    WIP on subnet available analyzer, not quite there yet (will likely wait until the collector is done to test it)

--- pkg/analyze/host_subnetavailable.go
+++ pkg/analyze/host_subnetavailable.go
@@ -17,9 +17,49 @@ func (a *AnalyzeHostSubnetAvailable) IsExcluded() (bool, error) {
 }
 
 func (a *AnalyzeHostSubnetAvailable) Analyze(getCollectedFileContents func(string) ([]byte, error)) ([]*AnalyzeResult, error) {
-	// TODO: implement
+	hostAnalyzer := a.hostAnalyzer
 
-	var coll resultCollector
+	result := &AnalyzeResult{
+		Title: a.Title(),
+	}
 
-	return coll.get(a.Title()), nil
+	for _, outcome := range hostAnalyzer.Outcomes {
+		if outcome.Fail != nil {
+			if outcome.Fail.When == "" {
+				result.IsFail = true
+				result.Message = outcome.Fail.Message
+				result.URI = outcome.Fail.URI
+
+				return []*AnalyzeResult{result}, nil
+			}
+
+			// TODO: implement
+			if string("TODO") == outcome.Fail.When {
+				result.IsFail = true
+				result.Message = outcome.Fail.Message
+				result.URI = outcome.Fail.URI
+
+				return []*AnalyzeResult{result}, nil
+			}
+		} else if outcome.Pass != nil {
+			if outcome.Pass.When == "" {
+				result.IsPass = true
+				result.Message = outcome.Pass.Message
+				result.URI = outcome.Pass.URI
+
+				return []*AnalyzeResult{result}, nil
+			}
+
+			// TODO: implement
+			if string("TODO") == outcome.Pass.When {
+				result.IsPass = true
+				result.Message = outcome.Pass.Message
+				result.URI = outcome.Pass.URI
+
+				return []*AnalyzeResult{result}, nil
+			}
+		}
+	}
+
+	return []*AnalyzeResult{result}, nil
 }
