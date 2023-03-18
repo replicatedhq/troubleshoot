4278b1ec06bc0dba08407ea2e4582d215342a28c
David Rohnow <davidr@replicated.com>
2023-03-18 09:30:20 -0700

    reconfig start -- adjusting certificates.go

--- pkg/collect/certificates.go
+++ pkg/collect/certificates.go
@@ -88,7 +88,8 @@ func (c *CollectCertificates) Collect(progressChan chan<- interface{}) (Collecto
 
 	// create JSON here
 	var certsJson = []byte("[]")
-	err := json.Unmarshal(certsJson, &results)
+	//err := json.Unmarshal(certsJson, &results)
+	err := json.Unmarshal([]byte(certsJson), &results)
 
 	if err != nil {
 		log.Println(err)
