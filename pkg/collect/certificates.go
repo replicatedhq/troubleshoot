b7d690588db55bbaa5d5f77f1c88ce94d7ef6fe4
David Rohnow <davidr@replicated.com>
2023-03-20 19:47:54 -0700

    reconfig start -- adjusting certificates.go

--- pkg/collect/certificates.go
+++ pkg/collect/certificates.go
@@ -32,7 +32,7 @@ type CollectCertificates struct {
 type CertCollection struct {
 	Source           *CertificateSource  `json:"source"`
 	Errors           []error             `json:"errors"`
-	CertificateChain []ParsedCertificate `json:"certificateChain"`
+	CertificateChain []ParsedCertificate `json:"certificateChain"`dafd
 }
 
 type CertificateSource struct {
