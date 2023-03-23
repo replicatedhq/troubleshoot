d2f77a04f737a4307c07fe7c87cc263d8f5236ab
David Rohnow <davidr@replicated.com>
2023-03-23 16:24:19 -0700

    certificates.go

--- pkg/collect/certificates.go
+++ pkg/collect/certificates.go
@@ -222,7 +222,7 @@ func CertParser(certName string, certs []byte) ([]ParsedCertificate, []string) {
 				continue // End here, start parsing the next cert in the for loop
 			}
 
-			certInfo = append(certInfo, ParsedCertificate{
+			certCollection := ParsedCertificate{
 				CertName:                certName,
 				Subject:                 parsedCert.Subject.ToRDNSequence().String(),
 				SubjectAlternativeNames: parsedCert.DNSNames,
@@ -231,7 +231,8 @@ func CertParser(certName string, certs []byte) ([]ParsedCertificate, []string) {
 				NotBefore:               parsedCert.NotBefore,
 				IsValid:                 currentTime.Before(parsedCert.NotAfter),
 				IsCA:                    parsedCert.IsCA,
-			})
+			}
+			certInfo = append(certInfo, certCollection)
 
 		}
 		log.Println("stuff should be here: ", certInfo)
