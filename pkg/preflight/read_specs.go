6de79afc35a514cd65294403f939a814e00c065b
Nathan Sullivan <nathans@replicated.com>
2023-05-16 11:44:54 +1000

    Search stdin for secrets with preflight specs (#1153)
    * we can now read preflight specs out of secrets, either from stdin or file input
    
    * moved spec read logic out into its own function so it can be unit
    tested easier
    
    * added more comprehensive unit testing on the different ways we can read in specs

---
+++ pkg/preflight/read_specs.go
@@ -0,0 +1,197 @@
+package preflight
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/replicatedhq/troubleshoot/cmd/util"
+	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
+	troubleshootclientsetscheme "github.com/replicatedhq/troubleshoot/pkg/client/troubleshootclientset/scheme"
+	"github.com/replicatedhq/troubleshoot/pkg/constants"
+	"github.com/replicatedhq/troubleshoot/pkg/docrewrite"
+	"github.com/replicatedhq/troubleshoot/pkg/oci"
+	"github.com/replicatedhq/troubleshoot/pkg/specs"
+	"github.com/replicatedhq/troubleshoot/pkg/types"
+	"gopkg.in/yaml.v2"
+	"k8s.io/client-go/kubernetes/scheme"
+)
+
+type documentHead struct {
+	Kind       string             `yaml:"kind"`
+	Metadata   documentMetadata   `yaml:"metadata,omitempty"`
+	Data       documentData       `yaml:"data,omitempty"`
+	StringData documentStringData `yaml:"stringData,omitempty"`
+}
+
+type documentMetadata struct {
+	Labels documentMetadataLabels `yaml:"labels,omitempty"`
+}
+
+type documentMetadataLabels struct {
+	TroubleshootKind string `yaml:"troubleshoot.io/kind,omitempty"`
+}
+
+type documentData struct {
+	PreflightYaml string `yaml:"preflight.yaml,omitempty"`
+}
+
+type documentStringData struct {
+	PreflightYaml string `yaml:"preflight.yaml,omitempty"`
+}
+
+type PreflightSpecs struct {
+	PreflightSpec     *troubleshootv1beta2.Preflight
+	HostPreflightSpec *troubleshootv1beta2.HostPreflight
+	UploadResultSpecs []*troubleshootv1beta2.Preflight
+}
+
+func (p *PreflightSpecs) Read(args []string) error {
+	var preflightContent []byte
+	var err error
+
+	for _, v := range args {
+		if strings.HasPrefix(v, "secret/") {
+			// format secret/namespace-name/secret-name
+			pathParts := strings.Split(v, "/")
+			if len(pathParts) != 3 {
+				return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, errors.Errorf("path %s must have 3 components", v))
+			}
+
+			spec, err := specs.LoadFromSecret(pathParts[1], pathParts[2], "preflight-spec")
+			if err != nil {
+				return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, errors.Wrap(err, "failed to get spec from secret"))
+			}
+
+			preflightContent = spec
+		} else if _, err = os.Stat(v); err == nil {
+			b, err := os.ReadFile(v)
+			if err != nil {
+				return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, err)
+			}
+
+			preflightContent = b
+		} else if v == "-" {
+			b, err := io.ReadAll(os.Stdin)
+			if err != nil {
+				return types.NewExitCodeError(constants.EXIT_CODE_CATCH_ALL, err)
+			}
+			preflightContent = b
+		} else {
+			u, err := url.Parse(v)
+			if err != nil {
+				return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, err)
+			}
+
+			if u.Scheme == "oci" {
+				content, err := oci.PullPreflightFromOCI(v)
+				if err != nil {
+					if err == oci.ErrNoRelease {
+						return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, errors.Errorf("no release found for %s.\nCheck the oci:// uri for errors or contact the application vendor for support.", v))
+					}
+
+					return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, err)
+				}
+
+				preflightContent = content
+			} else {
+				if !util.IsURL(v) {
+					return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, fmt.Errorf("%s is not a URL and was not found (err %s)", v, err))
+				}
+
+				req, err := http.NewRequest("GET", v, nil)
+				if err != nil {
+					// exit code: should this be catch all or spec issues...?
+					return types.NewExitCodeError(constants.EXIT_CODE_CATCH_ALL, err)
+				}
+				req.Header.Set("User-Agent", "Replicated_Preflight/v1beta2")
+				resp, err := http.DefaultClient.Do(req)
+				if err != nil {
+					// exit code: should this be catch all or spec issues...?
+					return types.NewExitCodeError(constants.EXIT_CODE_CATCH_ALL, err)
+				}
+				defer resp.Body.Close()
+
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, err)
+				}
+
+				preflightContent = body
+			}
+		}
+
+		multidocs := strings.Split(string(preflightContent), "\n---\n")
+
+		for _, doc := range multidocs {
+			var parsedDocHead documentHead
+
+			err := yaml.Unmarshal([]byte(doc), &parsedDocHead)
+			if err != nil {
+				return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, errors.Wrap(err, "failed to parse yaml"))
+			}
+
+			// We're going to look for either of "kind: Preflight" OR "kind: Secret" with the label "troubleshoot.io/kind: preflight"
+
+			if parsedDocHead.Kind != "Preflight" && parsedDocHead.Kind != "Secret" {
+				continue
+			}
+
+			if parsedDocHead.Kind == "Secret" {
+				if parsedDocHead.Metadata.Labels.TroubleshootKind == "preflight" {
+					secretSpecDoc := ""
+					// In a Secret, we need to get the document out of the data.`preflight.yaml` or stringData.`preflight.yaml` (stringData takes precedence)
+					if len(parsedDocHead.Data.PreflightYaml) > 0 {
+						b64DecPreflightYaml, err := base64.StdEncoding.DecodeString(parsedDocHead.Data.PreflightYaml)
+						if err != nil {
+							return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, errors.Wrap(err, "failed to base64 decode preflight secret data"))
+						}
+						secretSpecDoc = strings.TrimSpace(string(b64DecPreflightYaml))
+					}
+					// Do stringData second, if found overwrite data (as per K8s docs)
+					if len(parsedDocHead.StringData.PreflightYaml) > 0 {
+						secretSpecDoc = parsedDocHead.StringData.PreflightYaml
+					}
+					//
+					if len(secretSpecDoc) > 0 {
+						doc = secretSpecDoc
+					} else {
+						return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, errors.Wrap(err, "secret spec with preflight label found, but no preflight stringData or data?"))
+					}
+				} else {
+					// Not a preflight spec, skip
+					continue
+				}
+			}
+
+			preflightContent, err = docrewrite.ConvertToV1Beta2([]byte(doc))
+			if err != nil {
+				return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, errors.Wrap(err, "failed to convert to v1beta2"))
+			}
+
+			troubleshootclientsetscheme.AddToScheme(scheme.Scheme)
+			decode := scheme.Codecs.UniversalDeserializer().Decode
+			obj, _, err := decode([]byte(preflightContent), nil, nil)
+			if err != nil {
+				return types.NewExitCodeError(constants.EXIT_CODE_SPEC_ISSUES, errors.Wrapf(err, "failed to parse %s", v))
+			}
+
+			if spec, ok := obj.(*troubleshootv1beta2.Preflight); ok {
+				if spec.Spec.UploadResultsTo == "" {
+					p.PreflightSpec = ConcatPreflightSpec(p.PreflightSpec, spec)
+				} else {
+					p.UploadResultSpecs = append(p.UploadResultSpecs, spec)
+				}
+			} else if spec, ok := obj.(*troubleshootv1beta2.HostPreflight); ok {
+				p.HostPreflightSpec = ConcatHostPreflightSpec(p.HostPreflightSpec, spec)
+			}
+		}
+	}
+
+	return nil
+}
---
+++ pkg/preflight/read_specs_test.go
@@ -0,0 +1,299 @@
+package preflight
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/replicatedhq/troubleshoot/internal/testutils"
+	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type PreflightSpecsReadTests []PreflightSpecsReadTest
+
+type PreflightSpecsReadTest struct {
+	name          string
+	args          []string
+	customStdin   bool
+	stdinDataFile string
+	//
+	wantErr           bool
+	wantPreflightSpec *troubleshootv1beta2.Preflight
+	// TODOLATER: tests around this
+	wantHostPreflightSpec *troubleshootv1beta2.HostPreflight
+	// TODOLATER: tests around this
+	wantUploadResultSpecs []*troubleshootv1beta2.Preflight
+}
+
+func TestPreflightSpecsRead(t *testing.T) {
+	// NOTE: don't use t.Parallel(), these tests manipulate os.Stdin
+
+	// A very simple preflight spec
+	preflightFile := filepath.Join(testutils.FileDir(), "../../testdata/preflightspec/troubleshoot_v1beta2_preflight_gotest.yaml")
+	//
+	expectPreflightSpec := troubleshootv1beta2.Preflight{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Preflight",
+			APIVersion: "troubleshoot.sh/v1beta2",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "go-test-preflight",
+		},
+		Spec: troubleshootv1beta2.PreflightSpec{
+			UploadResultsTo: "",
+			Collectors: []*troubleshootv1beta2.Collect{
+				&troubleshootv1beta2.Collect{
+					Data: &troubleshootv1beta2.Data{
+						CollectorMeta: troubleshootv1beta2.CollectorMeta{
+							CollectorName: "",
+						},
+						Name: "example.json",
+						Data: `{
+  "foo": "bar",
+  "stuff": {
+    "foo": "bar",
+    "bar": true
+  },
+  "morestuff": [
+    {
+      "foo": {
+        "bar": 123
+      }
+    }
+  ]
+}
+`,
+					},
+				},
+			},
+			RemoteCollectors: []*troubleshootv1beta2.RemoteCollect(nil),
+			Analyzers: []*troubleshootv1beta2.Analyze{
+				&troubleshootv1beta2.Analyze{
+					JsonCompare: &troubleshootv1beta2.JsonCompare{
+						AnalyzeMeta: troubleshootv1beta2.AnalyzeMeta{
+							CheckName: "Compare JSON Example",
+						},
+						CollectorName: "",
+						FileName:      "example.json",
+						Path:          "morestuff.[0].foo.bar",
+						Value: `123
+`,
+						Outcomes: []*troubleshootv1beta2.Outcome{
+							&troubleshootv1beta2.Outcome{
+								Fail: &troubleshootv1beta2.SingleOutcome{
+									When:    "false",
+									Message: "The collected data does not match the value.",
+								},
+							},
+							&troubleshootv1beta2.Outcome{
+								Pass: &troubleshootv1beta2.SingleOutcome{
+									When:    "true",
+									Message: "The collected data matches the value.",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	/*
+		expectHostPreflightSpec := troubleshootv1beta2.HostPreflight{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "HostPreflight",
+					APIVersion: "troubleshoot.sh/troubleshootv1beta2",
+				},
+				Spec: troubleshootv1beta2.HostPreflightSpec{
+					Collectors:       []*troubleshootv1beta2.HostCollect(nil),
+					RemoteCollectors: []*troubleshootv1beta2.RemoteCollect(nil),
+					Analyzers:        []*troubleshootv1beta2.HostAnalyze(nil),
+				},
+			}
+	*/
+
+	// A more complexed preflight spec, which resides in a secret
+	preflightSecretFile := filepath.Join(testutils.FileDir(), "../../testdata/preflightspec/troubleshoot_v1beta2_preflight_secret_gotest.yaml")
+	//
+	expectSecretPreflightSpec := troubleshootv1beta2.Preflight{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Preflight",
+			APIVersion: "troubleshoot.sh/v1beta2",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "preflight-sample",
+		},
+		Spec: troubleshootv1beta2.PreflightSpec{
+			UploadResultsTo:  "",
+			RemoteCollectors: []*troubleshootv1beta2.RemoteCollect(nil),
+			Analyzers: []*troubleshootv1beta2.Analyze{
+				&troubleshootv1beta2.Analyze{
+					NodeResources: &troubleshootv1beta2.NodeResources{
+						AnalyzeMeta: troubleshootv1beta2.AnalyzeMeta{
+							CheckName: "Node Count Check",
+						},
+						Outcomes: []*troubleshootv1beta2.Outcome{
+							&troubleshootv1beta2.Outcome{
+								Fail: &troubleshootv1beta2.SingleOutcome{
+									When:    "count() < 3",
+									Message: "The cluster needs a minimum of 3 nodes.",
+								},
+							},
+							&troubleshootv1beta2.Outcome{
+								Pass: &troubleshootv1beta2.SingleOutcome{
+									Message: "There are not enough nodes to run this application (3 or more)",
+								},
+							},
+						},
+					},
+				},
+				&troubleshootv1beta2.Analyze{
+					ClusterVersion: &troubleshootv1beta2.ClusterVersion{
+						Outcomes: []*troubleshootv1beta2.Outcome{
+							&troubleshootv1beta2.Outcome{
+								Fail: &troubleshootv1beta2.SingleOutcome{
+									When:    "< 1.16.0",
+									Message: "The application requires at least Kubernetes 1.16.0, and recommends 1.18.0.",
+									URI:     "https://kubernetes.io",
+								},
+							},
+							&troubleshootv1beta2.Outcome{
+								Warn: &troubleshootv1beta2.SingleOutcome{
+									When:    "< 1.18.0",
+									Message: "Your cluster meets the minimum version of Kubernetes, but we recommend you update to 1.18.0 or later.",
+									URI:     "https://kubernetes.io",
+								},
+							},
+							&troubleshootv1beta2.Outcome{
+								Pass: &troubleshootv1beta2.SingleOutcome{
+									Message: "Your cluster meets the recommended and required versions of Kubernetes.",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	tests := PreflightSpecsReadTests{
+		// TODOLATER: URL support? local mock webserver? would prefer for these tests to not require internet :)
+		// TODOLATER: multidoc fixtures
+		PreflightSpecsReadTest{
+			name:                  "file-preflight",
+			args:                  []string{preflightFile},
+			customStdin:           false,
+			wantErr:               false,
+			wantPreflightSpec:     &expectPreflightSpec,
+			wantHostPreflightSpec: nil,
+			wantUploadResultSpecs: nil,
+		},
+		PreflightSpecsReadTest{
+			name:                  "file-secret",
+			args:                  []string{preflightSecretFile},
+			customStdin:           false,
+			wantErr:               false,
+			wantPreflightSpec:     &expectSecretPreflightSpec,
+			wantHostPreflightSpec: nil,
+			wantUploadResultSpecs: nil,
+		},
+		PreflightSpecsReadTest{
+			name:                  "stdin-preflight",
+			args:                  []string{"-"},
+			customStdin:           true,
+			stdinDataFile:         preflightFile,
+			wantErr:               false,
+			wantPreflightSpec:     &expectPreflightSpec,
+			wantHostPreflightSpec: nil,
+			wantUploadResultSpecs: nil,
+		},
+		PreflightSpecsReadTest{
+			name:                  "stdin-secret",
+			args:                  []string{"-"},
+			customStdin:           true,
+			stdinDataFile:         preflightSecretFile,
+			wantErr:               false,
+			wantPreflightSpec:     &expectSecretPreflightSpec,
+			wantHostPreflightSpec: nil,
+			wantUploadResultSpecs: nil,
+		},
+		/*
+			/* TODOLATER: needs a cluster with a spec installed?
+			PreflightSpecsReadTest{
+				name:     "cluster-secret",
+				args:     []string{"/secret/some-secret-spec"},
+				customStdin: false,
+				wantErr:  false,
+				wantPreflightSpec:     &expectSecretPreflightSpec,
+				wantHostPreflightSpec: &expectSecretHostPreflightSpec,
+				wantUploadResultsSpecs: expectSecretUploadResultSpecs,
+			},
+		*/
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			specs := PreflightSpecs{}
+
+			tErr := singleTestPreflightSpecsRead(t, &tt, &specs)
+
+			if tt.wantErr {
+				assert.Error(t, tErr)
+			} else {
+				require.NoError(t, tErr)
+			}
+
+			assert.Equal(t, specs.PreflightSpec, tt.wantPreflightSpec)
+			assert.Equal(t, specs.HostPreflightSpec, tt.wantHostPreflightSpec)
+			assert.Equal(t, specs.UploadResultSpecs, tt.wantUploadResultSpecs)
+		})
+	}
+}
+
+// Structured as a separate function so we can use defer appropriately
+func singleTestPreflightSpecsRead(t *testing.T, tt *PreflightSpecsReadTest, specs *PreflightSpecs) error {
+	var tmpfile *os.File
+	var err error
+	if tt.customStdin == true {
+		tmpfile, err = os.CreateTemp("", "singleTestPreflightSpecsRead")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer os.Remove(tmpfile.Name()) // clean up
+
+		// Read in data file
+		// TODOLATER: just copy the file...?
+		stdinData, err := os.ReadFile(tt.stdinDataFile)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := tmpfile.Write(stdinData); err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := tmpfile.Seek(0, 0); err != nil {
+			t.Fatal(err)
+		}
+
+		oldStdin := os.Stdin
+		defer func() { os.Stdin = oldStdin }() // Restore original Stdin
+
+		os.Stdin = tmpfile
+	}
+
+	err = specs.Read(tt.args)
+
+	if tt.customStdin == true {
+		if err = tmpfile.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return err
+}
