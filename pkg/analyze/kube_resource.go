4605f22b1b2eb63e174cad6fff9b48e66c6c0b19
danj-replicated <danj@replicated.com>
2023-06-06 12:55:42 +0100

    make CR analyzer compare ints and sizes

--- pkg/analyze/kube_resource.go
+++ pkg/analyze/kube_resource.go
@@ -5,7 +5,9 @@ import (
 	"path/filepath"
 	"reflect"
 	"strconv"
+	"strings"
 
+	humanize "github.com/dustin/go-humanize"
 	"github.com/pkg/errors"
 	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
 	"github.com/replicatedhq/troubleshoot/pkg/constants"
@@ -108,6 +110,132 @@ func FindResource(kind string, clusterScoped bool, namespace string, name string
 
 }
 
+func compareWhentoResource(w string, actual interface{}) (bool, error) {
+
+	// check our "when" has operators
+	var whenSplit []string
+	if strings.ContainsAny(w, "!=<>") {
+		whenSplit = strings.Split(strings.TrimSpace(w), " ")
+	} else {
+		return false, errors.New("no operators found")
+	}
+
+	// let's first check if we can cast "actual" as an int, that should inform us what comparison we're doing
+	actualAsInt, ok := actual.(int)
+	if ok {
+		// it's an int! we can do integer comparison here
+		// we're going to re-use an i'll-fitting bit of code from the deployment analyzer for now
+		return compareActualToWhen(w, actualAsInt)
+	}
+
+	// if we've fallen through here we're going to have to try a bit harder to work out what we're comparing
+	// let's try making it a string
+	actualAsString, ok := actual.(string)
+	if !ok {
+		return false, errors.New("could not cast found value as string")
+	}
+
+	actualAsSize, err := humanize.ParseBytes(actualAsString)
+	if err == nil {
+		// it's probably a size, we can do some comparison here
+		// but I'm being lazy here so we'll convert our last argument to an int and throw it back at our existing int comparison function
+		whenAsSize, err := humanize.ParseBytes(whenSplit[1])
+		if err != nil {
+			// our when wasn't a size! naughty user
+			return false, errors.New("Cannot compare size with not size")
+		}
+		whenIntAsString := strconv.FormatInt(int64(whenAsSize), 10)
+		return compareActualToWhen(whenSplit[0]+" "+whenIntAsString, int(actualAsSize))
+
+	}
+
+	return false, errors.New("could not match comparison method for result")
+}
+
+func analyzeWhenField(actual interface{}, outcomes []*troubleshootv1beta2.Outcome, checkName string) (*AnalyzeResult, error) {
+
+	result := &AnalyzeResult{
+		Title:   checkName,
+		IconKey: "kubernetes_text_analyze",
+		IconURI: "https://troubleshoot.sh/images/analyzer-icons/text-analyze.svg?w=13&h=16",
+	}
+
+	for _, outcome := range outcomes {
+		if outcome.Fail != nil {
+			if outcome.Fail.When != "" {
+				compareResult, err := compareWhentoResource(outcome.Fail.When, actual)
+				if err != nil {
+					return nil, errors.Wrapf(err, "failed to process when statement: %s", outcome.Fail.When)
+				}
+				if compareResult {
+					result.IsFail = true
+					result.Message = outcome.Fail.Message
+					result.URI = outcome.Fail.URI
+					return result, nil
+				}
+			} else {
+				result.IsFail = true
+				result.Message = outcome.Fail.Message
+				result.URI = outcome.Fail.URI
+
+				return result, nil
+			}
+		}
+		if outcome.Warn != nil {
+
+			if outcome.Warn.When != "" {
+				compareResult, err := compareWhentoResource(outcome.Fail.When, actual)
+				if err != nil {
+					return nil, errors.Wrapf(err, "failed to process when statement: %s", outcome.Warn.When)
+				}
+				if compareResult {
+					result.IsWarn = true
+					result.Message = outcome.Warn.Message
+					result.URI = outcome.Warn.URI
+					return result, nil
+				}
+			} else {
+				result.IsWarn = true
+				result.Message = outcome.Warn.Message
+				result.URI = outcome.Warn.URI
+
+				return result, nil
+			}
+
+
+		}
+		if outcome.Pass != nil {
+
+			if outcome.Pass.When != "" {
+				compareResult, err := compareWhentoResource(outcome.Pass.When, actual)
+				if err != nil {
+					return nil, errors.Wrapf(err, "failed to process when statement: %s", outcome.Pass.When)
+				}
+				if compareResult {
+					result.IsPass = true
+					result.Message = outcome.Pass.Message
+					result.URI = outcome.Pass.URI
+					return result, nil
+				}
+			}else {
+				result.IsPass = true
+				result.Message = outcome.Pass.Message
+				result.URI = outcome.Pass.URI
+
+				return result, nil
+			}
+		}
+	}
+
+	return &AnalyzeResult{
+		Title:   checkName,
+		IconKey: "kubernetes_text_analyze",
+		IconURI: "https://troubleshoot.sh/images/analyzer-icons/text-analyze.svg",
+		IsFail:  true,
+		Message: "Invalid analyzer",
+	}, nil
+}
+
 func (a *AnalyzeClusterResource) analyzeResource(analyzer *troubleshootv1beta2.ClusterResource, getFileContents getCollectedFileContents) (*AnalyzeResult, error) {
 	selected, err := FindResource(analyzer.Kind, analyzer.ClusterScoped, analyzer.Namespace, analyzer.Name, getFileContents)
 	if err != nil {
@@ -162,6 +290,15 @@ func (a *AnalyzeClusterResource) analyzeResource(analyzer *troubleshootv1beta2.C
 		}
 	} else if analyzer.RegexGroups != "" {
 		result, err := analyzeRegexGroups(analyzer.RegexGroups, actualYAML, analyzer.Outcomes, a.Title())
+		if err != nil {
+			return nil, err
+		}
+		if result != nil {
+			return result, nil
+		}
+	} else {
+		// fall through to comparing from the when key
+		result, err := analyzeWhenField(actual, analyzer.Outcomes, a.Title())
 		if err != nil {
 			return nil, err
 		}
