9c4e31af6304cf7aa666e70220680f83465587c6
Dexter Yan <yanshaocong@gmail.com>
2023-08-14 18:00:39 +1200

    feat(cluster_resources): update e2e tests

--- test/validate-support-bundle-e2e.sh
+++ test/validate-support-bundle-e2e.sh
@@ -41,7 +41,7 @@ fi
 base_path="$tmpdir/$bundle_directory_name/cluster-resources"
 folders=("auth-cani-list" "configmaps" "daemonsets" "endpoints" "events" "deployments" "leases" "services" "pvcs" "pvcs" "jobs" "roles" "statefulsets" "network-policy" "pods" "resource-quota" "rolebindings" "serviceaccounts")
 
-files=("volumeattachments")
+files=("namespaces" "volumeattachments" "pvs" "groups" "nodes" "priorityclasses" "resources")
 
 for folder in "${folders[@]}"; do
     if [ -d "$base_path/$folder" ]; then
@@ -61,7 +61,6 @@ done
 for file in "${files[@]}"; do
     if [ -e "$base_path/$file.json" ]
     then
-        cat "$base_path/$file.json"
         echo "$file.json file was collected"
     else
         echo "The $file.json file does not exist in $base_path path."
