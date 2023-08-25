27133445313146419d609d8a1f24370eb8292dcf
Dexter Yan <yanshaocong@gmail.com>
2023-08-25 16:25:30 +1200

    feat(helm): fix e2e test

--- test/e2e/support-bundle/cluster_pod_statuses_e2e_test.go
+++ test/e2e/support-bundle/cluster_pod_statuses_e2e_test.go
@@ -1,95 +1,95 @@
 package e2e
 
-// import (
-// 	"bytes"
-// 	"context"
-// 	"encoding/json"
-// 	"fmt"
-// 	"os"
-// 	"os/exec"
-// 	"strings"
-// 	"testing"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
 
-// 	"github.com/replicatedhq/troubleshoot/pkg/convert"
-// 	appsv1 "k8s.io/api/apps/v1"
-// 	corev1 "k8s.io/api/core/v1"
-// 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-// 	"sigs.k8s.io/e2e-framework/pkg/envconf"
-// 	"sigs.k8s.io/e2e-framework/pkg/features"
-// )
+	"github.com/replicatedhq/troubleshoot/pkg/convert"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+)
 
-// func TestPendingPod(t *testing.T) {
-// 	supportBundleName := "pod-deployment"
-// 	deploymentName := "test-pending-deployment"
-// 	containerName := "curl"
-// 	feature := features.New("Pending Pod Test").
-// 		Setup(func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
-// 			deployment := newDeployment(c.Namespace(), deploymentName, 1, containerName)
-// 			client, err := c.NewClient()
-// 			if err != nil {
-// 				t.Fatal(err)
-// 			}
-// 			if err = client.Resources().Create(ctx, deployment); err != nil {
-// 				t.Fatal(err)
-// 			}
+func TestPendingPod(t *testing.T) {
+	supportBundleName := "pod-deployment"
+	deploymentName := "test-pending-deployment"
+	containerName := "curl"
+	feature := features.New("Pending Pod Test").
+		Setup(func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+			deployment := newDeployment(c.Namespace(), deploymentName, 1, containerName)
+			client, err := c.NewClient()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err = client.Resources().Create(ctx, deployment); err != nil {
+				t.Fatal(err)
+			}
 
-// 			return ctx
-// 		}).
-// 		Assess("check support bundle catch pending pod", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
-// 			var out bytes.Buffer
-// 			var results []*convert.Result
+			return ctx
+		}).
+		Assess("check support bundle catch pending pod", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+			var out bytes.Buffer
+			var results []*convert.Result
 
-// 			tarPath := fmt.Sprintf("%s.tar.gz", supportBundleName)
-// 			targetFile := fmt.Sprintf("%s/analysis.json", supportBundleName)
+			tarPath := fmt.Sprintf("%s.tar.gz", supportBundleName)
+			targetFile := fmt.Sprintf("%s/analysis.json", supportBundleName)
 
-// 			cmd := exec.Command("../../../bin/support-bundle", "spec/pod.yaml", "--interactive=false", fmt.Sprintf("-o=%s", supportBundleName))
-// 			cmd.Stdout = &out
-// 			err := cmd.Run()
-// 			if err != nil {
-// 				t.Fatal(err)
-// 			}
+			cmd := exec.Command("../../../bin/support-bundle", "spec/pod.yaml", "--interactive=false", fmt.Sprintf("-o=%s", supportBundleName))
+			cmd.Stdout = &out
+			err := cmd.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
 
-// 			analysisJSON, err := readFileFromTar(tarPath, targetFile)
-// 			if err != nil {
-// 				t.Fatal(err)
-// 			}
+			analysisJSON, err := readFileFromTar(tarPath, targetFile)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-// 			err = json.Unmarshal(analysisJSON, &results)
-// 			if err != nil {
-// 				t.Fatal(err)
-// 			}
+			err = json.Unmarshal(analysisJSON, &results)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-// 			for _, result := range results {
-// 				if strings.Contains(result.Insight.Detail, deploymentName) {
-// 					return ctx
-// 				}
-// 			}
+			for _, result := range results {
+				if strings.Contains(result.Insight.Detail, deploymentName) {
+					return ctx
+				}
+			}
 
-// 			t.Fatal("Pending pod not found")
-// 			defer func() {
-// 				err := os.Remove(fmt.Sprintf("%s.tar.gz", supportBundleName))
-// 				if err != nil {
-// 					t.Fatal("Error remove file:", err)
-// 				}
-// 			}()
-// 			return ctx
-// 		}).Feature()
-// 	testenv.Test(t, feature)
-// }
+			t.Fatal("Pending pod not found")
+			defer func() {
+				err := os.Remove(fmt.Sprintf("%s.tar.gz", supportBundleName))
+				if err != nil {
+					t.Fatal("Error remove file:", err)
+				}
+			}()
+			return ctx
+		}).Feature()
+	testenv.Test(t, feature)
+}
 
-// func newDeployment(namespace string, name string, replicas int32, containerName string) *appsv1.Deployment {
-// 	labels := map[string]string{"app": "pending-test"}
-// 	return &appsv1.Deployment{
-// 		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
-// 		Spec: appsv1.DeploymentSpec{
-// 			Replicas: &replicas,
-// 			Selector: &metav1.LabelSelector{
-// 				MatchLabels: labels,
-// 			},
-// 			Template: corev1.PodTemplateSpec{
-// 				ObjectMeta: metav1.ObjectMeta{Labels: labels},
-// 				Spec:       corev1.PodSpec{Containers: []corev1.Container{{Name: containerName, Image: "nginx", Command: []string{"wge", "-O", "/work-dir/index.html", "https://www.wikipedia.org"}}}},
-// 			},
-// 		},
-// 	}
-// }
+func newDeployment(namespace string, name string, replicas int32, containerName string) *appsv1.Deployment {
+	labels := map[string]string{"app": "pending-test"}
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{Labels: labels},
+				Spec:       corev1.PodSpec{Containers: []corev1.Container{{Name: containerName, Image: "nginx", Command: []string{"wge", "-O", "/work-dir/index.html", "https://www.wikipedia.org"}}}},
+			},
+		},
+	}
+}
--- test/e2e/support-bundle/helm_collector_e2e_test.go
+++ test/e2e/support-bundle/helm_collector_e2e_test.go
@@ -1,16 +1,29 @@
 package e2e
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 	"sigs.k8s.io/e2e-framework/third_party/helm"
+
+	collect "github.com/replicatedhq/troubleshoot/pkg/collect"
+	"github.com/stretchr/testify/assert"
 )
 
+var curDir, _ = os.Getwd()
+
 func Test_HelmCollector(t *testing.T) {
+	releaseName := "nginx-release"
+
 	feature := features.New("Collector Helm Release").
 		Setup(func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
 			cluster, ok := envfuncs.GetKindClusterFromContext(ctx, ClusterName)
@@ -18,10 +31,47 @@ func Test_HelmCollector(t *testing.T) {
 				t.Fatalf("Failed to extract kind cluster %s from context", ClusterName)
 			}
 			manager := helm.New(cluster.GetKubeconfig())
-			err := manager.RunInstall(helm.WithName(ClusterName), helm.WithNamespace("default"), helm.WithChart("nginx"), helm.WithWait(), helm.WithTimeout("10m"))
+			err := manager.RunInstall(helm.WithName(releaseName), helm.WithNamespace(c.Namespace()), helm.WithChart(filepath.Join(curDir, "testdata/charts/nginx-15.2.0.tgz")), helm.WithWait(), helm.WithTimeout("1m"))
 			if err != nil {
-				t.Fatal("failed to invoke helm install operation due to an error", err)
+				fmt.Println(err)
 			}
+			return ctx
+		}).
+		Assess("check support bundle catch helm release", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+			var out bytes.Buffer
+			var results []collect.ReleaseInfo
+
+			supportBundleName := "helm-release"
+			namespace := c.Namespace()
+			tarPath := fmt.Sprintf("%s.tar.gz", supportBundleName)
+			targetFile := fmt.Sprintf("%s/helm/%s.json", supportBundleName, namespace)
+			cmd := exec.Command("../../../bin/support-bundle", "spec/helm.yaml", "--interactive=false", fmt.Sprintf("-o=%s", supportBundleName))
+			cmd.Stdout = &out
+			err := cmd.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			resultJSON, err := readFileFromTar(tarPath, targetFile)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			err = json.Unmarshal(resultJSON, &results)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.Equal(t, 1, len(results))
+			assert.Equal(t, releaseName, results[0].ReleaseName)
+			assert.Equal(t, "nginx", results[0].Chart)
+
+			defer func() {
+				err := os.Remove(fmt.Sprintf("%s.tar.gz", supportBundleName))
+				if err != nil {
+					t.Fatal("Error remove file:", err)
+				}
+			}()
 			return ctx
 		}).
 		Feature()
