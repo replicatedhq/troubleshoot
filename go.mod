e10e26adc9eb0f4404aca5b6aa7342f1f506f588
Archit Sharma <archit@pm.me>
2023-11-03 23:20:50 +0530

    leave it at that before helm v3 upgrades to oras v2
    Signed-off-by: Archit Sharma <archit@pm.me>

--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/mattn/go-isatty v0.0.19
 	github.com/mholt/archiver/v3 v3.5.1
 	github.com/microsoft/go-mssqldb v1.6.0
+	github.com/opencontainers/image-spec v1.1.0-rc4
 	github.com/oras-project/oras-credentials-go v0.3.0
 	github.com/pkg/errors v0.9.1
 	github.com/replicatedhq/termui/v3 v3.1.1-0.20200811145416-f40076d26851
@@ -89,7 +90,6 @@ require (
 	github.com/mistifyio/go-zfs/v3 v3.0.1 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
-	github.com/opencontainers/image-spec v1.1.0-rc4 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rubenv/sql-migrate v1.3.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
--- pkg/oci/pull.go
+++ pkg/oci/pull.go
@@ -4,26 +4,15 @@ import (
 	"context"
 	"fmt"
 	"net/url"
-	"os"
 	"strings"
 
-	// ocispec "github.com/opencontainers/image-spec/specs-go/v1"
-	"github.com/pkg/errors"
-
-	"oras.land/oras-go/v2/registry"
-
-	// "github.com/replicatedhq/troubleshoot/pkg/version"
-	// oras "oras.land/oras-go/v2"
-	// "oras.land/oras-go/v2/content"
-	// "oras.land/oras-go/v2/content/memory"
-	// "oras.land/oras-go/v2/content/oci"
-	// "oras.land/oras-go/v2/registry"
-
-	// "oras.land/oras-go/v2/registry/remote/retry"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	credentials "github.com/oras-project/oras-credentials-go"
+	"github.com/pkg/errors"
 	"oras.land/oras-go/v2"
 	"oras.land/oras-go/v2/content"
-	"oras.land/oras-go/v2/content/oci"
+	"oras.land/oras-go/v2/content/memory"
+	"oras.land/oras-go/v2/registry"
 	"oras.land/oras-go/v2/registry/remote"
 	"oras.land/oras-go/v2/registry/remote/auth"
 	"oras.land/oras-go/v2/registry/remote/retry"
@@ -43,21 +32,34 @@ func PullSupportBundleFromOCI(uri string) ([]byte, error) {
 
 func pullImageSpecsOCI(ctx context.Context, uri string, mediaType string, imageName string) ([]byte, error) {
 
-	// 0. Create an OCI layout store
-	tempDir, err := os.MkdirTemp("", "oci-layout-root")
-	if err != nil {
-		return []byte{}, err
-	}
+	// // 0. Create an OCI layout store
+	// tempDir, err := os.MkdirTemp("", "oci-layout-root")
+	// if err != nil {
+	// 	return []byte{}, err
+	// }
 
-	store, err := oci.New(tempDir)
-	if err != nil {
-		return []byte{}, err
-	}
+	// store, err := oci.New(tempDir)
+	// if err != nil {
+	// 	return []byte{}, err
+	// }
+
+	// https://pkg.go.dev/oras.land/oras-go/v2@v2.3.1/content/memory
+	memoryStore := memory.New()
+	// allowedMediaTypes := []string{
+	// 	mediaType,
+	// }
 
 	// 1. Connect to a remote repository
 	// ctx := context.Background()
-	reg := "registry.replicated.com"
-	repo, err := remote.NewRepository(reg + "/library/replicated")
+
+	// reg := "registry.replicated.com"
+	// repo, err := remote.NewRepository(reg + "/library/replicated")
+	parsedRef, err := parseURI(uri, imageName)
+	if err != nil {
+		return nil, err
+	}
+	repo, err := remote.NewRepository(parsedRef)
+
 	if err != nil {
 		return []byte{}, err
 	}
@@ -75,23 +77,56 @@ func pullImageSpecsOCI(ctx context.Context, uri string, mediaType string, imageN
 		Cache:      auth.DefaultCache,
 		Credential: credentials.Credential(credStore), // Use the credential store
 	}
-
 	// 3. Copy from the remote repository to the OCI layout store
 	tag := "1.0.0-beta.10"
-	manifestDescriptor, err := oras.Copy(ctx, repo, tag, store, tag, oras.DefaultCopyOptions)
+	// TODO: fetch all tags and choose the latest one;
+	// `latest` tag itself is deprecated now and requires `--version` on command line.
+	var descriptors, layers []ocispec.Descriptor
+	// opts = oras.CopyOptions{}
+	opts := oras.DefaultCopyOptions
+
+	manifest, err := oras.Copy(ctx, repo, tag, memoryStore, tag, opts)
 	if err != nil {
 		return []byte{}, err
 	}
+	descriptors = append(descriptors, manifest)
+	descriptors = append(descriptors, layers...)
 
-	fmt.Println("manifest pulled:", manifestDescriptor.Digest, manifestDescriptor.MediaType)
+	// expect 2 descriptors
+	if len(descriptors) != 2 {
+		return nil, fmt.Errorf("expected 2 descriptor, got %d", len(descriptors))
+	}
+
+	var matchingDescriptor *ocispec.Descriptor
+
+	for _, descriptor := range descriptors {
+		d := descriptor
+		switch d.MediaType {
+		case mediaType:
+			matchingDescriptor = &d
+		}
+	}
+	if matchingDescriptor == nil {
+		return nil, fmt.Errorf("no descriptor found with media type: %s", mediaType)
+	}
+
+	fmt.Println("manifest pulled:", manifest.Digest, manifest.MediaType)
+
+	// var fetched
+	contentReadCloser, _ := memoryStore.Fetch(ctx, *matchingDescriptor)
+	matchingSpec, err := content.ReadAll(contentReadCloser, *matchingDescriptor)
+	// matchingSpec.Read()
+	// if !ok {
+	// 	return nil, fmt.Errorf("failed to get matching descriptor")
+	// }
 
 	// 3. Fetch from OCI layout store to verify
-	fetched, err := content.FetchAll(ctx, store, manifestDescriptor)
+	// fetched, err := content.FetchAll(ctx, memoryStore, manifest)
 	if err != nil {
 		return []byte{}, err
 	}
-	fmt.Printf("manifest content:\n%s", fetched)
-	return fetched, nil
+	// fmt.Printf("manifest content:\n%s", fetched)
+	return matchingSpec, nil
 }
 
 // PullSpecsFromOCI pulls both the preflight and support bundle specs from the given URI
