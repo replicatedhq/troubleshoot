f78d7db95c2c26edc0c1efaf3bc3c913ab429da5
Gerard Nguyen <gerard@replicated.com>
2024-05-02 20:23:05 +1000

    fix unit tests from code review

--- cmd/troubleshoot/cli/run_test.go
+++ cmd/troubleshoot/cli/run_test.go
@@ -18,7 +18,8 @@ import (
 	testclient "k8s.io/client-go/kubernetes/fake"
 )
 
-var orig = `
+func templSpec() string {
+	return `
 apiVersion: troubleshoot.sh/v1beta2
 kind: SupportBundle
 metadata:
@@ -30,6 +31,7 @@ spec:
         name: kube-root-ca.crt
         namespace: default
 `
+}
 
 func Test_loadSupportBundleSpecsFromURIs(t *testing.T) {
 	// Run a webserver to serve the spec
@@ -45,7 +47,7 @@ spec:
 	}))
 	defer srv.Close()
 
-	orig := strings.ReplaceAll(orig, "$MY_URI", srv.URL)
+	orig := strings.ReplaceAll(templSpec(), "$MY_URI", srv.URL)
 
 	ctx := context.Background()
 	kinds, err := loader.LoadSpecs(ctx, loader.LoadOptions{RawSpec: orig})
@@ -115,14 +117,15 @@ spec:
 	}))
 	defer srv.Close()
 
-	orig := strings.ReplaceAll(orig, "$MY_URI", srv.URL)
+	orig := strings.ReplaceAll(templSpec(), "$MY_URI", srv.URL)
 	specUri := testutils.ServeFromFilePath(t, orig)
 	specs := []string{specFile, specUri}
 
 	sb, _, err := loadSpecs(ctx, specs, client)
 	require.NoError(t, err)
 	assert.NotNil(t, sb.Spec.Collectors[0].Logs)
-	assert.Nil(t, sb.Spec.Collectors[1].ConfigMap)
+	assert.Nil(t, sb.Spec.Collectors[1].ConfigMap)      // original spec gone
+	assert.NotNil(t, sb.Spec.Collectors[1].ClusterInfo) // new spec from URI
 }
 
 func Test_loadSupportBundleSpecsFromURIs_TimeoutError(t *testing.T) {
@@ -133,7 +136,7 @@ func Test_loadSupportBundleSpecsFromURIs_TimeoutError(t *testing.T) {
 	ctx := context.Background()
 
 	kinds, err := loader.LoadSpecs(ctx, loader.LoadOptions{
-		RawSpec: strings.ReplaceAll(orig, "$MY_URI", srv.URL),
+		RawSpec: strings.ReplaceAll(templSpec(), "$MY_URI", srv.URL),
 	})
 	require.NoError(t, err)
 
