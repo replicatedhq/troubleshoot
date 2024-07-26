05f2ba3fd2054e3afab9e315d58f9190ba72b5a6
Gerard Nguyen <gerard@replicated.com>
2024-07-26 13:37:36 +1000

    new schema for etcd collector

--- pkg/apis/troubleshoot/v1beta2/collector_shared.go
+++ pkg/apis/troubleshoot/v1beta2/collector_shared.go
@@ -299,6 +299,10 @@ type DNS struct {
 	Timeout       string `json:"timeout,omitempty" yaml:"timeout,omitempty"`
 }
 
+type Etcd struct {
+	CollectorMeta `json:",inline" yaml:",inline"`
+}
+
 type Collect struct {
 	ClusterInfo      *ClusterInfo      `json:"clusterInfo,omitempty" yaml:"clusterInfo,omitempty"`
 	ClusterResources *ClusterResources `json:"clusterResources,omitempty" yaml:"clusterResources,omitempty"`
@@ -329,6 +333,7 @@ type Collect struct {
 	Sonobuoy         *Sonobuoy         `json:"sonobuoy,omitempty" yaml:"sonobuoy,omitempty"`
 	NodeMetrics      *NodeMetrics      `json:"nodeMetrics,omitempty" yaml:"nodeMetrics,omitempty"`
 	DNS              *DNS              `json:"dns,omitempty" yaml:"dns,omitempty"`
+	Etcd             *Etcd             `json:"etcd,omitempty" yaml:"etcd,omitempty"`
 }
 
 func (c *Collect) AccessReviewSpecs(overrideNS string) []authorizationv1.SelfSubjectAccessReviewSpec {
