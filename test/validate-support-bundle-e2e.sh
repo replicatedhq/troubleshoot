ad93a6828c03e51c36967ec21a0a19b037bdfc50
Diamon Wiggins <diamonw757@gmail.com>
2024-08-23 13:09:43 -0400

    add e2e tests for support-bundle commands with no args

--- test/validate-support-bundle-e2e.sh
+++ test/validate-support-bundle-e2e.sh
@@ -115,6 +115,53 @@ if ! grep "labelled-support-bundle-4 \*\*\*HIDDEN\*\*\*" "$tmpdir/$bundle_direct
     exit 1
 fi
 
+echo "======= Generating support bundle from k8s cluster using 0 arguments and a spec in the cluster ======"
+recreate_tmpdir
+kubectl apply -f "$PRJ_ROOT/testdata/supportbundle/labelled-specs"
+./bin/support-bundle -v1 --interactive=false --output=$tmpdir/$bundle_archive_name
+if [ $? -ne 0 ]; then
+    echo "support-bundle command failed"
+    exit $?
+fi
+
+if ! tar -xvzf $tmpdir/$bundle_archive_name --directory $tmpdir; then
+    echo "A valid support bundle archive was not generated"
+    exit 1
+fi
+
+if ! grep "labelled-support-bundle-1 \*\*\*HIDDEN\*\*\*" "$tmpdir/$bundle_directory_name/echo-hi-1"; then
+    echo "$(cat $tmpdir/$bundle_directory_name/echo-hi-1)"
+    echo "Hidden content not found in redacted echo-hi-1 file"
+    exit 1
+fi
+
+if ! grep "labelled-support-bundle-2 \*\*\*HIDDEN\*\*\*" "$tmpdir/$bundle_directory_name/echo-hi-2"; then
+    echo "$(cat $tmpdir/$bundle_directory_name/echo-hi-2)"
+    echo "Hidden content not found in redacted echo-hi-2 file"
+    exit 1
+fi
+
+if ! grep "labelled-support-bundle-3 \*\*\*HIDDEN\*\*\*" "$tmpdir/$bundle_directory_name/echo-hi-3"; then
+    echo "$(cat $tmpdir/$bundle_directory_name/echo-hi-3)"
+    echo "Hidden content not found in redacted echo-hi-3 file"
+    exit 1
+fi
+
+if ! grep "labelled-support-bundle-4 \*\*\*HIDDEN\*\*\*" "$tmpdir/$bundle_directory_name/echo-hi-4"; then
+    echo "$(cat $tmpdir/$bundle_directory_name/echo-hi-4)"
+    echo "Hidden content not found in redacted echo-hi-4 file"
+    exit 1
+fi
+
+echo "======= Generating support bundle from k8s cluster using 0 arguments and no spec in the cluster ======"
+recreate_tmpdir
+kubectl apply -f "$PRJ_ROOT/testdata/supportbundle/labelled-specs"
+./bin/support-bundle -v1 --interactive=false --output=$tmpdir/$bundle_archive_name
+if [ $? -ne 0 ]; then
+    echo "support-bundle command should have failed"
+    exit $?
+fi
+
 echo "======= Generating support bundle from k8s secret/<namespace-name>/<secret-name>/<data-key> ======"
 recreate_tmpdir
 kubectl apply -f "$PRJ_ROOT/testdata/supportbundle/labelled-specs"
