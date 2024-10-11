0c0146eca15d51104c72587df65261f650797337
Diamon Wiggins <diamonw757@gmail.com>
2024-10-11 14:57:12 -0400

    initial refactor of host os analyzer

--- pkg/analyze/host_os_info.go
+++ pkg/analyze/host_os_info.go
@@ -17,9 +17,13 @@ type AnalyzeHostOS struct {
 	hostAnalyzer *troubleshootv1beta2.HostOSAnalyze
 }
 
-type NodeOSInfo struct {
+type NodesInfo struct {
+	Nodes []string `json:"nodes"`
+}
+
+type RemoteCollectContent struct {
 	NodeName string
-	collect.HostOSInfo
+	Content  interface{}
 }
 
 func (a *AnalyzeHostOS) Title() string {
@@ -33,7 +37,7 @@ func (a *AnalyzeHostOS) IsExcluded() (bool, error) {
 func (a *AnalyzeHostOS) Analyze(
 	getCollectedFileContents func(string) ([]byte, error), findFiles getChildCollectedFileContents,
 ) ([]*AnalyzeResult, error) {
-	var nodesOSInfo []NodeOSInfo
+	var remoteCollectContents []RemoteCollectContent
 	result := AnalyzeResult{}
 	result.Title = a.Title()
 
@@ -46,13 +50,12 @@ func (a *AnalyzeHostOS) Analyze(
 			return []*AnalyzeResult{&result}, errors.Wrap(err, "failed to get collected file")
 		}
 
-		var nodes collect.HostOSInfoNodes
-		if err := json.Unmarshal(contents, &nodes); err != nil {
+		var nodeNames NodesInfo
+		err = json.Unmarshal(contents, &nodeNames)
+		if err != nil {
 			return []*AnalyzeResult{&result}, errors.Wrap(err, "failed to unmarshal host os info nodes")
 		}
-
-		// iterate over each node and analyze the host os info
-		for _, node := range nodes.Nodes {
+		for _, node := range nodeNames.Nodes {
 			contents, err := getCollectedFileContents(collect.NodeInfoBaseDir + "/" + node + "/" + collect.HostInfoFileName)
 			if err != nil {
 				return []*AnalyzeResult{&result}, errors.Wrap(err, "failed to get collected file")
@@ -63,10 +66,15 @@ func (a *AnalyzeHostOS) Analyze(
 				return []*AnalyzeResult{&result}, errors.Wrap(err, "failed to unmarshal host os info")
 			}
 
-			nodesOSInfo = append(nodesOSInfo, NodeOSInfo{NodeName: node, HostOSInfo: osInfo})
+			remoteCollectContent := RemoteCollectContent{
+				NodeName: node,
+				Content:  osInfo,
+			}
+
+			remoteCollectContents = append(remoteCollectContents, remoteCollectContent)
 		}
 
-		results, err := analyzeOSVersionResult(nodesOSInfo, a.hostAnalyzer.Outcomes, a.Title())
+		results, err := analyzeOSVersionResult(remoteCollectContents, a.hostAnalyzer.Outcomes, a.Title())
 		if err != nil {
 			return []*AnalyzeResult{&result}, errors.Wrap(err, "failed to analyze os version result")
 		}
@@ -77,21 +85,92 @@ func (a *AnalyzeHostOS) Analyze(
 	if err := json.Unmarshal(contents, &osInfo); err != nil {
 		return []*AnalyzeResult{&result}, errors.Wrap(err, "failed to unmarshal host os info")
 	}
-	nodesOSInfo = append(nodesOSInfo, NodeOSInfo{NodeName: "", HostOSInfo: osInfo})
-	return analyzeOSVersionResult(nodesOSInfo, a.hostAnalyzer.Outcomes, a.Title())
+	remoteCollectContents = append(remoteCollectContents, RemoteCollectContent{NodeName: "", Content: osInfo})
+	return analyzeOSVersionResult(remoteCollectContents, a.hostAnalyzer.Outcomes, a.Title())
 }
 
-func analyzeOSVersionResult(nodesOSInfo []NodeOSInfo, outcomes []*troubleshootv1beta2.Outcome, title string) ([]*AnalyzeResult, error) {
+func analyzeOSVersionResult(collectedContent []RemoteCollectContent, outcomes []*troubleshootv1beta2.Outcome, title string) ([]*AnalyzeResult, error) {
 	var results []*AnalyzeResult
-	for _, osInfo := range nodesOSInfo {
-		if title == "" {
-			title = "Host OS Info"
+	for _, osInfo := range collectedContent {
+
+		currentTitle := title
+		if osInfo.NodeName != "" {
+			currentTitle = fmt.Sprintf("%s - Node %s", title, osInfo.NodeName)
 		}
 
-		analyzeResult, err := analyzeByOutcomes(outcomes, osInfo, title)
+		osInfo, ok := osInfo.Content.(collect.HostOSInfo)
+		if !ok {
+			return nil, errors.New("a valid host os info was not found")
+		}
+
+		checkCondition := func(when string) (bool, error) {
+			parts := strings.Split(when, " ")
+			if len(parts) < 3 {
+				return false, errors.New("when condition must have at least 3 parts")
+			}
+			expectedVer := fixVersion(parts[2])
+			toleratedVer, err := semver.ParseTolerant(expectedVer)
+			if err != nil {
+				return false, errors.Wrapf(err, "failed to parse version: %s", expectedVer)
+			}
+			when = fmt.Sprintf("%s %v", parts[1], toleratedVer)
+			whenRange, err := semver.ParseRange(when)
+			if err != nil {
+				return false, errors.Wrapf(err, "failed to parse version range: %s", when)
+			}
+
+			// Match the kernel version regardless of the platform
+			// e.g "kernelVersion == 4.15"
+			if parts[0] == "kernelVersion" {
+				fixedKernelVer := fixVersion(osInfo.KernelVersion)
+				toleratedKernelVer, err := semver.ParseTolerant(fixedKernelVer)
+				if err != nil {
+					return false, errors.Wrapf(err, "failed to parse tolerant: %v", fixedKernelVer)
+				}
+				if whenRange(toleratedKernelVer) {
+					return true, nil
+				}
+			}
+
+			// Match the platform version and and kernel version passed in as
+			// "<platform>-<kernelVersion>-kernel" e.g "centos-8.2-kernel == 8.2"
+			platform := parts[0]
+			kernelInfo := fmt.Sprintf("%s-%s-kernel", osInfo.Platform, osInfo.PlatformVersion)
+			if len(strings.Split(platform, "-")) == 3 && strings.Split(platform, "-")[2] == "kernel" {
+				if platform == kernelInfo {
+					fixedKernelVer := fixVersion(osInfo.KernelVersion)
+					toleratedKernelVer, err := semver.ParseTolerant(fixedKernelVer)
+					if err != nil {
+						return false, errors.Wrapf(err, "failed to parse tolerant: %v", fixedKernelVer)
+					}
+					if whenRange(toleratedKernelVer) {
+						return true, nil
+					}
+				}
+				// Match the platform version
+				// e.g "centos == 8.2"
+			} else if platform == osInfo.Platform {
+				fixedDistVer := fixVersion(osInfo.PlatformVersion)
+				toleratedDistVer, err := semver.ParseTolerant(fixedDistVer)
+				if err != nil {
+					return false, errors.Wrapf(err, "failed to parse tolerant: %v", fixedDistVer)
+				}
+				if whenRange(toleratedDistVer) {
+					return true, nil
+				}
+			}
+			return false, nil
+		}
+
+		analyzeResult, err := evaluateOutcomes(outcomes, checkCondition, currentTitle)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to evaluate outcomes")
+		}
+
+		/*analyzeResult, err := analyzeByOutcomes(outcomes, osInfo, title)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to analyze condition")
-		}
+		}*/
 		results = append(results, analyzeResult...)
 	}
 
@@ -114,105 +193,78 @@ func fixVersion(versionStr string) string {
 	return version
 }
 
-func analyzeByOutcomes(outcomes []*troubleshootv1beta2.Outcome, osInfo NodeOSInfo, title string) ([]*AnalyzeResult, error) {
+func evaluateOutcomes(outcomes []*troubleshootv1beta2.Outcome, checkCondition func(string) (bool, error), title string) ([]*AnalyzeResult, error) {
 	var results []*AnalyzeResult
-	for _, outcome := range outcomes {
-		if osInfo.NodeName != "" {
-			title = fmt.Sprintf("%s - Node %s", title, osInfo.NodeName)
-		}
 
+	for _, outcome := range outcomes {
 		result := AnalyzeResult{
 			Title: title,
 		}
-		when := ""
-		message := ""
-		uri := ""
-
 		if outcome.Fail != nil {
-			result.IsFail = true
-			when = outcome.Fail.When
-			message = outcome.Fail.Message
-			uri = outcome.Fail.URI
-		} else if outcome.Warn != nil {
-			result.IsWarn = true
-			when = outcome.Warn.When
-			message = outcome.Warn.Message
-			uri = outcome.Warn.URI
-		} else if outcome.Pass != nil {
-			result.IsPass = true
-			when = outcome.Pass.When
-			message = outcome.Pass.Message
-			uri = outcome.Pass.URI
-		} else {
-			return nil, errors.New("empty outcome")
-		}
-
-		result.Message = message
-		result.URI = uri
-		// When is usually empty as the final case and should be treated as true
-		if when == "" {
-			results = append(results, &result)
-			return results, nil
-		}
-
-		parts := strings.Split(when, " ")
-		if len(parts) < 3 {
-			return []*AnalyzeResult{&result}, errors.New("when condition must have at least 3 parts")
-		}
-		expectedVer := fixVersion(parts[2])
-		toleratedVer, err := semver.ParseTolerant(expectedVer)
-		if err != nil {
-			return []*AnalyzeResult{&result}, errors.Wrapf(err, "failed to parse version: %s", expectedVer)
-		}
-		when = fmt.Sprintf("%s %v", parts[1], toleratedVer)
-		whenRange, err := semver.ParseRange(when)
-		if err != nil {
-			return []*AnalyzeResult{&result}, errors.Wrapf(err, "failed to parse version range: %s", when)
-		}
-
-		// Match the kernel version regardless of the platform
-		// e.g "kernelVersion == 4.15"
-		if parts[0] == "kernelVersion" {
-			fixedKernelVer := fixVersion(osInfo.KernelVersion)
-			toleratedKernelVer, err := semver.ParseTolerant(fixedKernelVer)
-			if err != nil {
-				return []*AnalyzeResult{}, errors.Wrapf(err, "failed to parse tolerant: %v", fixedKernelVer)
-			}
-			if whenRange(toleratedKernelVer) {
+			if outcome.Fail.When == "" {
+				result.IsFail = true
+				result.Message = outcome.Fail.Message
+				result.URI = outcome.Fail.URI
 				results = append(results, &result)
 				return results, nil
 			}
-		}
 
-		// Match the platform version and and kernel version passed in as
-		// "<platform>-<kernelVersion>-kernel" e.g "centos-8.2-kernel == 8.2"
-		platform := parts[0]
-		kernelInfo := fmt.Sprintf("%s-%s-kernel", osInfo.Platform, osInfo.PlatformVersion)
-		if len(strings.Split(platform, "-")) == 3 && strings.Split(platform, "-")[2] == "kernel" {
-			if platform == kernelInfo {
-				fixedKernelVer := fixVersion(osInfo.KernelVersion)
-				toleratedKernelVer, err := semver.ParseTolerant(fixedKernelVer)
-				if err != nil {
-					return []*AnalyzeResult{&result}, errors.Wrapf(err, "failed to parse tolerant: %v", fixedKernelVer)
-				}
-				if whenRange(toleratedKernelVer) {
-					results = append(results, &result)
-					return results, nil
-				}
-			}
-			// Match the platform version
-			// e.g "centos == 8.2"
-		} else if platform == osInfo.Platform {
-			fixedDistVer := fixVersion(osInfo.PlatformVersion)
-			toleratedDistVer, err := semver.ParseTolerant(fixedDistVer)
+			isMatch, err := checkCondition(outcome.Fail.When)
 			if err != nil {
-				return []*AnalyzeResult{&result}, errors.Wrapf(err, "failed to parse tolerant: %v", fixedDistVer)
+				return []*AnalyzeResult{&result}, errors.Wrapf(err, "failed to compare %s", outcome.Fail.When)
 			}
-			if whenRange(toleratedDistVer) {
+
+			if isMatch {
+				result.IsFail = true
+				result.Message = outcome.Fail.Message
+				result.URI = outcome.Fail.URI
+				results = append(results, &result)
+				return results, nil
+			}
+		} else if outcome.Warn != nil {
+			if outcome.Warn.When == "" {
+				result.IsWarn = true
+				result.Message = outcome.Warn.Message
+				result.URI = outcome.Warn.URI
+				results = append(results, &result)
+				return results, nil
+			}
+
+			isMatch, err := checkCondition(outcome.Warn.When)
+			if err != nil {
+				return []*AnalyzeResult{&result}, errors.Wrapf(err, "failed to compare %s", outcome.Warn.When)
+			}
+
+			if isMatch {
+				result.IsWarn = true
+				result.Message = outcome.Warn.Message
+				result.URI = outcome.Warn.URI
+				results = append(results, &result)
+				return results, nil
+			}
+		} else if outcome.Pass != nil {
+			if outcome.Pass.When == "" {
+				result.IsPass = true
+				result.Message = outcome.Pass.Message
+				result.URI = outcome.Pass.URI
+				results = append(results, &result)
+				return results, nil
+			}
+
+			isMatch, err := checkCondition(outcome.Pass.When)
+			if err != nil {
+				return []*AnalyzeResult{&result}, errors.Wrapf(err, "failed to compare %s", outcome.Pass.When)
+			}
+
+			if isMatch {
+				result.IsPass = true
+				result.Message = outcome.Pass.Message
+				result.URI = outcome.Pass.URI
 				results = append(results, &result)
 				return results, nil
 			}
 		}
 	}
-	return results, nil
+
+	return nil, nil
 }
