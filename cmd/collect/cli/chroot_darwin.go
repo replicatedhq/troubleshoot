1ffa8b069f8f9fcb68e75e69bdedf2438033a485
hedge-sparrow <sparrow@spooky.academy>
2024-10-22 16:48:48 +0100

    Add friendly chroot warning if running without elevated permissions

--- cmd/collect/cli/chroot_darwin.go
+++ cmd/collect/cli/chroot_darwin.go
@@ -1,12 +1,18 @@
 package cli
 
 import (
+	"errors"
 	"syscall"
+
+	"github.com/replicatedhq/troubleshoot/internal/util"
 )
 
 func checkAndSetChroot(newroot string) error {
 	if newroot == "" {
 		return nil
+	}
+	if !util.IsRunningAsRoot() {
+		return errors.New("Can only chroot when run as root")
 	}
 	if err := syscall.Chroot(newroot); err != nil {
 		return err
