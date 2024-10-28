fb4c3389cc56aab7128532c62376dd3f663c4915
hedge-sparrow <sparrow@spooky.academy>
2024-10-28 13:17:47 +0000

    Refactor host cpu analyzer for remote collection

--- pkg/analyze/host_cpu.go
+++ pkg/analyze/host_cpu.go
@@ -2,6 +2,8 @@ package analyzer
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
 	"slices"
 	"strconv"
 	"strings"
@@ -33,94 +35,45 @@ func (a *AnalyzeHostCPU) IsExcluded() (bool, error) {
 	return isExcluded(a.hostAnalyzer.Exclude)
 }
 
-func (a *AnalyzeHostCPU) Analyze(
-	getCollectedFileContents func(string) ([]byte, error), findFiles getChildCollectedFileContents,
-) ([]*AnalyzeResult, error) {
-	hostAnalyzer := a.hostAnalyzer
+func (a *AnalyzeHostCPU) CheckCondition(when string, data collectorData) (bool, error) {
+	rawData, ok := data.([]byte)
 
-	contents, err := getCollectedFileContents(collect.HostCPUPath)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get collected file")
+	if !ok {
+		return false, fmt.Errorf("expected data to be []uint8 (raw bytes), got: %v", reflect.TypeOf(data))
 	}
 
 	cpuInfo := collect.CPUInfo{}
-	if err := json.Unmarshal(contents, &cpuInfo); err != nil {
-		return nil, errors.Wrap(err, "failed to unmarshal cpu info")
+	if err := json.Unmarshal(rawData, &cpuInfo); err != nil {
+		return false, fmt.Errorf("failed to unmarshal data into CPUInfo: %v", err)
 	}
 
-	result := AnalyzeResult{
-		Title: a.Title(),
+	return compareHostCPUConditionalToActual(when, cpuInfo.LogicalCount, cpuInfo.PhysicalCount, cpuInfo.Flags, cpuInfo.MachineArch)
+
+}
+
+func (a *AnalyzeHostCPU) Analyze(
+	getCollectedFileContents func(string) ([]byte, error), findFiles getChildCollectedFileContents,
+) ([]*AnalyzeResult, error) {
+	result := AnalyzeResult{Title: a.Title()}
+
+	// Use the generic function to collect both local and remote data
+	collectedContents, err := retrieveCollectedContents(
+		getCollectedFileContents,
+		collect.HostCPUPath,        // Local path
+		collect.NodeInfoBaseDir,    // Remote base directory
+		collect.HostMemoryFileName, // Remote file name
+	)
+	if err != nil {
+		return []*AnalyzeResult{&result}, err
 	}
 
-	for _, outcome := range hostAnalyzer.Outcomes {
-
-		if outcome.Fail != nil {
-			if outcome.Fail.When == "" {
-				result.IsFail = true
-				result.Message = outcome.Fail.Message
-				result.URI = outcome.Fail.URI
-
-				return []*AnalyzeResult{&result}, nil
-			}
-
-			isMatch, err := compareHostCPUConditionalToActual(outcome.Fail.When, cpuInfo.LogicalCount, cpuInfo.PhysicalCount, cpuInfo.Flags, cpuInfo.MachineArch)
-			if err != nil {
-				return nil, errors.Wrap(err, "failed to compare")
-			}
-
-			if isMatch {
-				result.IsFail = true
-				result.Message = outcome.Fail.Message
-				result.URI = outcome.Fail.URI
-
-				return []*AnalyzeResult{&result}, nil
-			}
-		} else if outcome.Warn != nil {
-			if outcome.Warn.When == "" {
-				result.IsWarn = true
-				result.Message = outcome.Warn.Message
-				result.URI = outcome.Warn.URI
-
-				return []*AnalyzeResult{&result}, nil
-			}
-
-			isMatch, err := compareHostCPUConditionalToActual(outcome.Warn.When, cpuInfo.LogicalCount, cpuInfo.PhysicalCount, cpuInfo.Flags, cpuInfo.MachineArch)
-			if err != nil {
-				return nil, errors.Wrap(err, "failed to compare")
-			}
-
-			if isMatch {
-				result.IsWarn = true
-				result.Message = outcome.Warn.Message
-				result.URI = outcome.Warn.URI
-
-				return []*AnalyzeResult{&result}, nil
-			}
-		} else if outcome.Pass != nil {
-			if outcome.Pass.When == "" {
-				result.IsPass = true
-				result.Message = outcome.Pass.Message
-				result.URI = outcome.Pass.URI
-
-				return []*AnalyzeResult{&result}, nil
-			}
-
-			isMatch, err := compareHostCPUConditionalToActual(outcome.Pass.When, cpuInfo.LogicalCount, cpuInfo.PhysicalCount, cpuInfo.Flags, cpuInfo.MachineArch)
-			if err != nil {
-				return nil, errors.Wrap(err, "failed to compare")
-			}
-
-			if isMatch {
-				result.IsPass = true
-				result.Message = outcome.Pass.Message
-				result.URI = outcome.Pass.URI
-
-				return []*AnalyzeResult{&result}, nil
-			}
-		}
+	results, err := analyzeHostCollectorResults(collectedContents, a.hostAnalyzer.Outcomes, a.CheckCondition, a.Title())
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to analyze OS version")
 	}
 
-	return []*AnalyzeResult{&result}, nil
+	return results, nil
+
 }
 
 func doCompareHostCPUMicroArchitecture(microarch string, flags []string) (res bool, err error) {
