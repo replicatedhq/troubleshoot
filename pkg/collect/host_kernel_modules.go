20b34c0ef125856fcce713301b65251d587a670c
Ethan Mosbaugh <ethan@replicated.com>
2025-02-18 11:23:52 -0800

    fix(host-preflights): buildtin kernel modules file from wrong path

--- pkg/collect/host_kernel_modules.go
+++ pkg/collect/host_kernel_modules.go
@@ -228,7 +228,7 @@ func (l kernelModulesLoaded) collectBuiltin() (map[string]KernelModuleInfo, erro
 	}
 	kernel := strings.TrimSpace(string(out))
 
-	file, err := os.Open(fmt.Sprintf("/usr/lib/modules/%s/modules.builtin", kernel))
+	file, err := os.Open(fmt.Sprintf("/lib/modules/%s/modules.builtin", kernel))
 	if err != nil {
 		if os.IsNotExist(err) {
 			return nil, nil
