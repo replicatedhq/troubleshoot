856fdeaf0d2336a2e5a868bcc3311d8270d541bd
Noah Campbell <noah.edward.campbell@gmail.com>
2025-10-06 14:32:29 -0700

    Improved error handling for support bundles and redactors for windows (#1878)
    * improved error handling and window locking
    
    * Delete all-windows-collectors.yaml
    
    * addressing bugbot concerns
    
    * Update host_tcpportstatus.go
    
    * Update redact.go

--- pkg/collect/host_tcpportstatus.go
+++ pkg/collect/host_tcpportstatus.go
@@ -50,13 +50,11 @@ func (c *CollectHostTCPPortStatus) Collect(progressChan chan<- interface{}) (map
 		dialAddress = fmt.Sprintf("%s:%d", ip, c.hostCollector.Port)
 	}
 
-	networkStatus, err := checkTCPConnection(progressChan, listenAddress, dialAddress, 10*time.Second)
-	if err != nil {
-		return nil, err
-	}
+	networkStatus, errorMessage, checkErr := checkTCPConnection(progressChan, listenAddress, dialAddress, 10*time.Second)
 
 	result := NetworkStatusResult{
-		Status: networkStatus,
+		Status:  networkStatus,
+		Message: errorMessage,
 	}
 	b, err := json.Marshal(result)
 	if err != nil {
@@ -74,7 +72,7 @@ func (c *CollectHostTCPPortStatus) Collect(progressChan chan<- interface{}) (map
 
 	return map[string][]byte{
 		name: b,
-	}, nil
+	}, checkErr
 }
 
 func getIPv4FromInterface(iface *net.Interface) (net.IP, error) {
