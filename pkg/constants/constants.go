280a582f4f5d1dcb242dc9e4441ba1797b0209c7
Ethan Mosbaugh <ethan@replicated.com>
2025-11-26 21:39:07 -0800

    debug

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -56,7 +56,7 @@ const (
 	CLUSTER_RESOURCES_CLUSTER_ROLE_BINDINGS       = "clusterrolebindings"
 	CLUSTER_RESOURCES_PRIORITY_CLASS              = "priorityclasses"
 	CLUSTER_RESOURCES_ENDPOINTS                   = "endpoints"
-	CLUSTER_RESOURCES_ENDPOINTSICES               = "endpointslices"
+	CLUSTER_RESOURCES_ENDPOINTSLICES              = "endpointslices"
 	CLUSTER_RESOURCES_SERVICE_ACCOUNTS            = "serviceaccounts"
 	CLUSTER_RESOURCES_LEASES                      = "leases"
 	CLUSTER_RESOURCES_VOLUME_ATTACHMENTS          = "volumeattachments"
